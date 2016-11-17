de3e77267bf68d874c4fa2fd8bfdd1c3efadbc63
Richard <richard.jovelin@oicr.on.ca>
2016-11-16 21:03:32 -0500

    find orthologs of nested genes in chimp and macaque

--- CountOrthologs.py
+++ CountOrthologs.py
@@ -46,6 +46,7 @@ CfaGFF = 'Canis_familiaris.CanFam3.1.86.gff3'
 PtrGFF = 'Pan_troglodytes.CHIMP2.1.4.86.gff3'
 Mca = 'Macaca_mulatta.Mmul_8.0.1.86.gff3' 
 
+
 # make lists of host-nested gene pairs
 HumanHostNestedPairs = GetHostNestedPairs(HumanHostGenes)
 print('host-gene pairs in human', len(HumanHostNestedPairs))
@@ -68,68 +69,78 @@ MouseDogOrthos = ParseOrthologFile('MouseDogOrthologs.txt')
 ChimpMacaqueOrthos = ParseOrthologFile('ChimpMacaqueOrthologs.txt')
 HumanMacaqueOrthos = ParseOrthologFile('HumanMacaqueOrthologs.txt')
 
-
+HumanChimpMacaqueOrthos = ParseOrthologFile('HumanChimpMacaqueOrthologs.txt')
+print('human-chimp-macaque', len(HumanChimpMacaqueOrthos))
 
 HsaOrthoPtr, HsaOrthoMmu, HsaOrthoCfa, PtrOrthoMmu, HsaOrthoMca, PtrOrthoMca = 0, 0, 0, 0, 0, 0
 HsaMmuPtrOrtho, HsaMmuCfaOrtho, HsaMcaPtrOrtho = 0, 0, 0
 
+HsaPtrMcaOrthos = 0
+
+NestedHomolgy = []
+
 for pair in HumanHostNestedPairs:
-    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos:
-        HsaOrthoMmu += 1
-    if pair[0] in HumanDogOrthos and pair[1] in HumanDogOrthos:
-        HsaOrthoCfa += 1
-    if pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
-        HsaOrthoPtr += 1
-    if pair[0] in HumanMacaqueOrthos and pair[1] in HumanMacaqueOrthos:
-        HsaOrthoMca += 1
-    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos and pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
-        HsaMmuPtrOrtho += 1
-    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos and pair[0] in HumanDogOrthos and pair[1] in HumanDogOrthos:
-        HsaMmuCfaOrtho += 1
-    if pair[0] in HumanMacaqueOrthos and pair[1] in HumanMacaqueOrthos and pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
-        HsaMcaPtrOrtho += 1
+    if pair[0] in HumanChimpMacaqueOrthos and pair[1] in HumanChimpMacaqueOrthos:
+        HsaPtrMcaOrthos += 1
+        NestedHomolgy.append(pair)
+        
+    
+    
+#    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos:
+#        HsaOrthoMmu += 1
+#    if pair[0] in HumanDogOrthos and pair[1] in HumanDogOrthos:
+#        HsaOrthoCfa += 1
+#    if pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
+#        HsaOrthoPtr += 1
+#    if pair[0] in HumanMacaqueOrthos and pair[1] in HumanMacaqueOrthos:
+#        HsaOrthoMca += 1
+#    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos and pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
+#        HsaMmuPtrOrtho += 1
+#    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos and pair[0] in HumanDogOrthos and pair[1] in HumanDogOrthos:
+#        HsaMmuCfaOrtho += 1
+#    if pair[0] in HumanMacaqueOrthos and pair[1] in HumanMacaqueOrthos and pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
+#        HsaMcaPtrOrtho += 1
     
 
-print('ortho human-mouse', HsaOrthoMmu)
-print('ortho human-dog', HsaOrthoCfa)
-print('ortho human-chimp', HsaOrthoPtr)
-print('ortho human-mouse-dog', HsaMmuCfaOrtho)
-print('ortho human-mouse-chimp', HsaMmuPtrOrtho)
-print('ortho human-chimp-macaque', HsaMcaPtrOrtho)
+#print('ortho human-mouse', HsaOrthoMmu)
+#print('ortho human-dog', HsaOrthoCfa)
+#print('ortho human-chimp', HsaOrthoPtr)
+#print('ortho human-mouse-dog', HsaMmuCfaOrtho)
+#print('ortho human-mouse-chimp', HsaMmuPtrOrtho)
+#print('ortho human-chimp-macaque', HsaMcaPtrOrtho)
 
-PtrOrthoMmu = 0
-for pair in ChimpHostNestedPairs:
-    if pair[0] in ChimpMouseOrthos and pair[1] in ChimpMouseOrthos:
-        PtrOrthoMmu += 1
-    if pair[0] in ChimpMacaqueOrthos and pair[1] in ChimpMacaqueOrthos:
-        PtrOrthoMca += 1
-print('orthos chimp-mouse', PtrOrthoMmu)
-print('orthos chimp-macaque', PtrOrthoMca)
+print('ortho human-chimp-macaque', HsaPtrMcaOrthos)
+
+
+
+
+#PtrOrthoMmu = 0
+#for pair in ChimpHostNestedPairs:
+#    if pair[0] in ChimpMouseOrthos and pair[1] in ChimpMouseOrthos:
+#        PtrOrthoMmu += 1
+#    if pair[0] in ChimpMacaqueOrthos and pair[1] in ChimpMacaqueOrthos:
+#        PtrOrthoMca += 1
+#print('orthos chimp-mouse', PtrOrthoMmu)
+#print('orthos chimp-macaque', PtrOrthoMca)
 
 # make set of host and nested genes
 chimphostcontained = MakeHostNestedGeneSet(ChimpContained)
 mousehostcontained = MakeHostNestedGeneSet(MouseContained)
 chimphostnested = MakeHostNestedGeneSet(ChimpHostGenes)
 mousehostnested = MakeHostNestedGeneSet(MouseHostGenes)
-
+macaquehostnested = MakeHostNestedGeneSet(MacaqueHostGenes)
+macaquehostcontained = MakeHostNestedGeneSet(MacaqueContained)
 
 hsaspecific = 0
 hsaspnotcontained = 0
 hsaspecontained = 0
 
 # get the human specific nested pairs
-for pair in HumanHostNestedPairs:
-    if pair[0] in HumanMouseOrthos and pair[1] in HumanMouseOrthos and pair[0] in HumanChimpOrthos and pair[1] in HumanChimpOrthos:
-        if HumanMouseOrthos[pair[0]][0] not in mousehostnested and HumanMouseOrthos[pair[1]][0] not in mousehostnested:
-            if HumanChimpOrthos[pair[0]][0] not in chimphostnested and HumanChimpOrthos[pair[1]][0] not in chimphostnested:
-                hsaspecific += 1
-                if (HumanMouseOrthos[pair[0]][0] in mousehostcontained and HumanMouseOrthos[pair[1]][0] in mousehostcontained) or (HumanChimpOrthos[pair[0]][0] in chimphostcontained and HumanChimpOrthos[pair[1]][0] in chimphostcontained):
-                    hsaspecontained += 1
-                elif HumanMouseOrthos[pair[0]][0] not in mousehostcontained and HumanMouseOrthos[pair[1]][0] not in mousehostcontained and HumanChimpOrthos[pair[0]][0] not in chimphostcontained and HumanChimpOrthos[pair[1]][0] not in chimphostcontained:
-                    hsaspnotcontained += 1
-print('human specific', hsaspecific)
+for pair in NestedHomolgy:
+    if HumanChimpMacaqueOrthos[pair[0]][0][0] not in chimphostcontained and HumanChimpMacaqueOrthos[pair[1]][0][0] not in chimphostcontained and HumanChimpMacaqueOrthos[pair[0]][1][0] not in macaquehostcontained and HumanChimpMacaqueOrthos[pair[1]][1][0] not in macaquehostcontained:
+        hsaspnotcontained += 1
+    
 print('human specific not contained', hsaspnotcontained)
-print('human specific contained', hsaspecontained)            
             
                    
         
