c80cae8b70ab4b9c57488bf208723f8940623090
Richard <richard.jovelin@oicr.on.ca>
2016-11-22 13:42:53 -0500

    perform chi2 tests

--- MakeTableEnrichmentIntronlessNested.py
+++ MakeTableEnrichmentIntronlessNested.py
@@ -61,9 +61,10 @@ SpeciesNames = ['human', 'chimp', 'gorilla', 'orangoutan', 'macaque']
 HostGenes = [HumanHostGenes, ChimpHostGenes, GorillaHostGenes, OrangOutanHostGenes, MacaqueHostGenes]
 
 
-# create lists to store the number of introns for the longest transcript of the host,
-# nested and un-nested genes in each species [[human], [chimp], [gorilla], [orang-outan], [macaque]]
-HostIntron, NestedIntron, OthersIntron = [], [], []
+# create lists to store the number of intronless nested genes and nested genes
+# with introns that are on the same strand and opposite strand with their hosts
+# [human, chimp, gorilla, orang-outan, macaque]
+IntronlessSame, IntronlessOpposite, IntronSame, IntronOpposite = [], [], [], []
 
 
 # loop over GFF files, find nested and intronic=nested genes in each species 
@@ -94,148 +95,51 @@ for i in range(len(GFFs)):
     SpMatches = MatchHostTranscriptWithNestedTranscript(HostGenes[i], SpMapGeneTranscript, SpGeneLongestTranscript, SpTranscriptCoordinates, SpIntronCoord)
     # list all host, nested transcript pairs [[host, nested]]
     SpHostNestedPairs = GetHostNestedPairs(SpMatches)
+    # count nested intronless-same, intronless-opposite, intron-same, intron-opposite
+    NoIntronSame, NoIntronOpposite, WithIntronSame, WithIntronOpposite = 0, 0, 0, 0
+    # loop over host-nested transcript pairs
+    for i in range(len(SpHostNestedPairs)):
+        # get the orientation of each transcript [+,+]
+        OrientationPair = GenePairOrientation(SpHostNestedPairs[i], SpTranscriptCoordinates)
+        # determine if the nested transcript has introns
+        if SpHostNestedPairs[i][1] in SpIntronCoord:
+            IntronLess = False
+        else:
+            IntronLess = True
+        # check presence of intron and orientation of host and nested transcripts
+        if IntronLess == True and len(set(OrientationPair)) == 1:
+            # intronless, same orientation
+            NoIntronSame += 1
+        elif IntronLess == True and len(set(OrientationPair)) == 2:
+            # intronless, differente orientation
+            NoIntronOpposite += 1
+        elif IntronLess == False and len(set(OrientationPair)) == 1:
+            # with introns and same orientation
+            WithIntronSame += 1
+        elif IntronLess == False and len(set(OrientationPair)) == 2:
+            # with introns and opposite orientation
+            WithIntronOpposite += 1
+    # populate lists
+    IntronlessSame.append(NoIntronSame)
+    IntronlessOpposite.append(NoIntronOpposite)
+    IntronSame.append(WithIntronSame)
+    IntronOpposite.append(WithIntronOpposite)      
 
-
-# count nested intronless-same, intronless-opposite, intron-same, intron-opposite
-CelIntronlessSame, CelIntronlessOpposite, CelIntronSame, CelIntronOpposite = 0, 0, 0, 0
-# loop over host-nested transcript pairs
-for i in range(len(CelHostNestedPairs)):
-    # get the orientation of each transcript [+,+]
-    OrientationPair = GenePairOrientation(CelHostNestedPairs[i], CelTranscriptCoordinates)
-    # determine if the nested transcript has introns
-    if CelHostNestedPairs[i][1] in CelIntronCoordinates:
-        IntronLess = False
-    else:
-        IntronLess = True
-    if IntronLess == True and len(set(OrientationPair)) == 1:
-        # intronless, same orientation
-        CelIntronlessSame += 1
-    elif IntronLess == True and len(set(OrientationPair)) == 2:
-        # intronless, differente orientation
-        CelIntronlessOpposite += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 1:
-        # with introns and same orientation
-        CelIntronSame += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 2:
-        # with introns and opposite orientation
-        CelIntronOpposite += 1
-
-
-# get the coordinates of briggsae genes on each chromo
-# {chromo: {gene:[chromosome, start, end, sense]}}
-CbrGeneCoordChromo = ChromoGenesCoord(CbrGFF)
-# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-CbrGeneCoord = FromChromoCoordToGeneCoord(CbrGeneCoordChromo)
-# get briggsae expression {wormbase ID gene: [expression at 10 stages]}
-CbrExpressionStage = ExpressionDevelopemtStages('WBPaper00041190.cbg.mr.csv')
-# get the coordinates of introns # {chromo: {transcript:[(intron_start, intron_end), ...]}}
-CbrIntronCoordChromo = CDSIntronCoord(CbrGFF, 'intron')
-# create a dict {transcript: [(intron_start, intron_end), ...]}
-CbrIntronCoordinates = {}
-for i in CbrIntronCoordChromo:
-    for j in CbrIntronCoordChromo[i]:
-        CbrIntronCoordinates[j] = copy.deepcopy(CbrIntronCoordChromo[i][j])
-# Map Transcript names to gene names {transcript: gene}
-CbrMapTranscriptGene = TranscriptToGene(CbrGFF)
-# List all transcripts for each gene {gene: [transcripts]}
-CbrMapGeneTranscript = GeneToTranscripts(CbrGFF)
-# get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
-CbrTranscriptCoordinates = TranscriptsCoord(CbrGFF)
-# map genes to their longest transcript {gene: longest_transcript}
-CbrGeneLongestTranscript = LongestTranscript(CbrTranscriptCoordinates, CbrMapGeneTranscript)
-# match longest transcript of the nested genes to transcript of the host gene (longest transcript in priority)
-CbrMatches = MatchHostTranscriptWithNestedTranscript(CbrHostGenes, CbrMapGeneTranscript, CbrGeneLongestTranscript, CbrTranscriptCoordinates, CbrIntronCoordinates)
-# list all host, nested transcript pairs [[host, nested]]
-CbrHostNestedPairs = GetHostNestedPairs(CbrMatches)
-
-# count nested intronless-same, intronless-opposite, intron-same, intron-opposite
-CbrIntronlessSame, CbrIntronlessOpposite, CbrIntronSame, CbrIntronOpposite = 0, 0, 0, 0
-# loop over host-nested transcript pairs
-for i in range(len(CbrHostNestedPairs)):
-    # get the orientation of each transcript [+,+]
-    OrientationPair = GenePairOrientation(CbrHostNestedPairs[i], CbrTranscriptCoordinates)
-    # determine if the nested transcript has introns
-    if CbrHostNestedPairs[i][1] in CbrIntronCoordinates:
-        IntronLess = False
-    else:
-        IntronLess = True
-    if IntronLess == True and len(set(OrientationPair)) == 1:
-        # intronless, same orientation
-        CbrIntronlessSame += 1
-    elif IntronLess == True and len(set(OrientationPair)) == 2:
-        # intronless, differente orientation
-        CbrIntronlessOpposite += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 1:
-        # with introns and same orientation
-        CbrIntronSame += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 2:
-        # with introns and opposite orientation
-        CbrIntronOpposite += 1
-
-
-# get the coordinates of remanei gene on each chromo
-# {chromo: {gene:[chromosome, start, end, sense]}}
-CrmGeneCoordChromo = CrmClaChromoGenesCoord(CrmGFF)
-# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-CrmGeneCoord = FromChromoCoordToGeneCoord(CrmGeneCoordChromo)
-# get remanei expression {wormbase ID gene: [expression at 10 stages]}
-CrmExpression = ExpressionDevelopemtStages('WBPaper00041190.cre.mr.csv')
-# get expression for genes with gene names e dict {CRE_gene_name: [expression values]}
-CrmExpressionStage = MapCrmGeneNamesIDs(CrmExpression, 'c_remanei.PRJNA53967.WS256.geneIDs.txt')
-# get the coordinates of introns {chromo: {transcript:[(intron_start, intron_end), ...]}}
-CrmIntronCoordChromo = CrmClaCDSIntronCoord(CrmGFF, 'intron')
-# create a dict {transcript: [(intron_start, intron_end), ...]}
-CrmIntronCoordinates = {}
-for i in CrmIntronCoordChromo:
-    for j in CrmIntronCoordChromo[i]:
-        CrmIntronCoordinates[j] = copy.deepcopy(CrmIntronCoordChromo[i][j])
-# Map transcripts to their parent gene {transcript: gene}
-CrmMapTranscriptGene = CrmClaTranscriptToGene(CrmGFF)
-# Get the list of transcripts for each parent gene {gene: [transcripts]}
-CrmMapGeneTranscript = CrmClaGeneToTranscripts(CrmMapTranscriptGene)
-# get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
-CrmTranscriptCoordinates = CrmClaTranscriptCoord(CrmGFF)
-# map genes to their longest transcript {gene: longest_transcript}
-CrmGeneLongestTranscript = LongestTranscript(CrmTranscriptCoordinates, CrmMapGeneTranscript)
-# match host transcripts with nested transcripts 
-CrmMatches = CrmClaMatchHostTSNestedTS(CrmHostGenes, CrmTranscriptCoordinates, CrmIntronCoordinates)
-# list all host, nested transcript pairs [[host, nested]]
-CrmHostNestedPairs = GetHostNestedPairs(CrmHostGenes)
-
-# count nested intronless-same, intronless-opposite, intron-same, intron-opposite
-CrmIntronlessSame, CrmIntronlessOpposite, CrmIntronSame, CrmIntronOpposite = 0, 0, 0, 0
-# loop over host-nested transcript pairs
-for i in range(len(CrmHostNestedPairs)):
-    # get the orientation of each transcript [+,+]
-    OrientationPair = GenePairOrientation(CrmHostNestedPairs[i], CrmTranscriptCoordinates)
-    # determine if the nested transcript has introns
-    if CrmHostNestedPairs[i][1] in CrmIntronCoordinates:
-        IntronLess = False
-    else:
-        IntronLess = True
-    if IntronLess == True and len(set(OrientationPair)) == 1:
-        # intronless, same orientation
-        CrmIntronlessSame += 1
-    elif IntronLess == True and len(set(OrientationPair)) == 2:
-        # intronless, differente orientation
-        CrmIntronlessOpposite += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 1:
-        # with introns and same orientation
-        CrmIntronSame += 1
-    elif IntronLess == False and len(set(OrientationPair)) == 2:
-        # with introns and opposite orientation
-        CrmIntronOpposite += 1
-
+# create a list to store the P values, in the same order as the lists with number of nested genes
+PVal = []
 # perform statistical tests to find out if there is an over-representation of intronless genes in same orientation
-CelP = stats.fisher_exact([[CelIntronlessSame, CelIntronlessOpposite], [CelIntronSame, CelIntronOpposite]])[1]
-CbrP = stats.fisher_exact([[CbrIntronlessSame, CbrIntronlessOpposite], [CbrIntronSame, CbrIntronOpposite]])[1]
-CrmP = stats.fisher_exact([[CrmIntronlessSame, CrmIntronlessOpposite], [CrmIntronSame, CrmIntronOpposite]])[1]
+for i in range(len(SpeciesNames)):
+    P = stats.fisher_exact([[IntronlessSame[i], IntronlessOpposite[i]], [IntronSame[i], IntronOpposite[i]]])[1]
+    PVal.append(P)
 
 # create table
 # species, intronless-same, intronless-opposite, intron-same, intron-opposite, Pval
 newfile = open('ContingencyTableIntronlessGenes.txt', 'w')
 header = '\t'.join(['Species', 'Intronless-Same', 'Intronless-Opposite', 'Intron-Same', 'Intron-Opposite', 'P'])
-newfile.write('\t'.join(['C. elegans', str(CelIntronlessSame), str(CelIntronlessOpposite), str(CelIntronSame), str(CelIntronOpposite), str(round(CelP, 4))])+'\n')
-newfile.write('\t'.join(['C. briggsae', str(CbrIntronlessSame), str(CbrIntronlessOpposite), str(CbrIntronSame), str(CbrIntronOpposite), str(round(CbrP, 4))])+'\n')
-newfile.write('\t'.join(['C. remanei', str(CrmIntronlessSame), str(CrmIntronlessOpposite), str(CrmIntronSame), str(CrmIntronOpposite), str(round(CrmP, 4))])+'\n')
+newfile.write(header + '\n')
+
+# loop over species:
+for i in range(len(SpeciesNames)):
+    line = '\t'.join([SpeciesNames[0], str(IntronlessSame[i]), str(IntronlessOpposite[i]), str(IntronSame[i]), str(IntronOpposite[i])])
+    newfile.write(line + '\n')
 newfile.close()
