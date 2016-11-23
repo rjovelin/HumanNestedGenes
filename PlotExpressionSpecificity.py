96c2c12d4a96fa4ff553bdbe893f0645bf1df6f8
Richard <richard.jovelin@oicr.on.ca>
2016-11-22 22:51:37 -0500

    edited distance requirements and computed speciicity

--- PlotExpressionSpecificity.py
+++ PlotExpressionSpecificity.py
@@ -58,7 +58,8 @@ SpeciesNames = ['human', 'chimp', 'gorilla', 'orangoutan', 'macaque']
 # make a list of host:nested genes dictionaries
 HostGenes = [HumanHostGenes, ChimpHostGenes, GorillaHostGenes, OrangOutanHostGenes, MacaqueHostGenes]
 
-
+# make parallel lists to store expression specificity of host and nested genes [[human], [chimp], [gorilla], [orangutan], [macaque]]
+HostSpecificity, NestedSpecificity = [], []
 
 # loop over GFF files, find nested and intronic=nested genes in each species 
 for i in range(len(GFFs)):
@@ -80,22 +81,88 @@ for i in range(len(GFFs)):
     SpExpression = RemoveGenesLackingExpression(SpExpression)
     # get relative expression
     SpExpression = TransformRelativeExpression(SpExpression)
+    # compute expression specificity for all genes
+    SpTau = ExpressionSpecificity(SpExpression)    
     # generate un-nested gene pairs on ech chromosome with length < 500 bp
     SpControlPairs = GenerateUnNestedGenePairs(HostGenes[i], SpGeneCoord, SpOrderedGenes, SpExpression)
     # make a list of host-nested gene pairs
-    SpHostNestedPairs = GetHostNestedPairs(HostGenes[i])    
-    # print number of genes on each chromo
-    chromohost = {}
-    expressedpairs = 0
+    SpHostNestedPairs = GetHostNestedPairs(HostGenes[i])
+    # remove gene pairs with genes lacking expression
+    SpHostNestedPairs = FilterGenePairsWithoutExpression(SpHostNestedPairs, SpExpression)
+    # create lists to store specificity for host and nested genes for that species
+    tauhost, taunested = [], []
+    # loop over gene pairs:
     for pair in SpHostNestedPairs:
-        if pair[0] in SpExpression and pair[1] in SpExpression:
-            chromo = SpGeneCoord[pair[0]][0]
-            if chromo in chromohost:
-                chromohost[chromo] += 1
-            else:
-                chromohost[chromo] = 1
-    for chromo in chromohost:
-        print(chromo, chromohost[chromo], len(SpControlPairs[chromo]))
+        # populate lists
+        tauhost.append(SpTau[pair[0]])
+        taunested.append(SpTau[pair[1]])
+    HostSpecificity.append(tauhost)
+    NestedSpecificity.append(taunested)
+
+
+# make lists of gene specificity for each species     
+HumanTau = [HostSpecificity[0], NestedSpecificity[0]]
+ChimpTau = [HostSpecificity[1], NestedSpecificity[1]]
+GorillaTau = [HostSpecificity[2], NestedSpecificity[2]]
+OrangutanTau = [HostSpecificity[3], HostSpecificity[3]]
+Macaque = [HostSpecificity[4], HostSpecificity[4]]
+
+# make a list with all the list data
+AllData = [HumanTau, ChimpTau, GorillaTau, OrangutanTau, MacaqueTau]
+
+# create a function to get the mean and SEM of items in a list
+def GetMeanSEM(L):
+    '''
+    (list) -> (list, list)
+    Take a list of inner lists of numbers and return a list with mean values
+    and a parallel list with SEM values for each item of the outter list
+    '''
+    # create lists of mean and SEM
+    MeanVal, SEMVal = [], []
+    # loop over the outter ist
+    for i in range(len(L)):
+        MeanVal.append(np.mean(L[i]))
+        SEMVal.append(np.std(L[i]) / math.sqrt(len(L[i])))
+    return MeanVal, SEMVal
+    
+# create lists with means and with SEM
+HumanMeans, HumanSEM = GetMeanSEM(HumanTau)
+ChimpMeans, ChimpSEM = GetMeanSEM(ChimpTau)
+GorillaMeans, GorillaSEM = GetMeanSEM(GorillaTau)
+OrangutanMeans, OrangutanSEM = GetMeanSEM(OrangutanTau)
+MacaqueMeans, MacaqueSEM = GetMeanSEM(MacaqueTau)
+
+# perform statistical tests between gene categories in all species
+# create dict to store results {species: [P_host-nested, P_host-unnested, P_nested-unnested]}
+PValues = {}
+# loop over inner lists in data list
+for i in range(len(AllData)):
+    # initialize dict with empty list
+    PValues[SpeciesNames[i]] = []
+    # loop over inner list, compare gene categories
+    for j in range(0, len(AllData[i]) -1):
+        for k in range(j+1, len(AllData[i])):
+            P = stats.ranksums(AllData[i][j], AllData[i][k])[1]
+            PValues[SpeciesNames[i]].append(P)
+# print p values
+for sp in PValues:
+    print(sp, PValues[sp])
+
+
+
+
+#    # print number of genes on each chromo
+#    chromohost = {}
+#    expressedpairs = 0
+#    for pair in SpHostNestedPairs:
+#        if pair[0] in SpExpression and pair[1] in SpExpression:
+#            chromo = SpGeneCoord[pair[0]][0]
+#            if chromo in chromohost:
+#                chromohost[chromo] += 1
+#            else:
+#                chromohost[chromo] = 1
+#    for chromo in chromohost:
+#        print(chromo, chromohost[chromo], len(SpControlPairs[chromo]))
         
 
 
