95ec06d88eb1759190bc79fa6fa979dd499085a7
Richard <richard.jovelin@oicr.on.ca>
2016-11-29 22:36:42 -0500

    align human-chimp orthologous CDS

--- AlignCDSSequences.py
+++ AlignCDSSequences.py
@@ -12,15 +12,9 @@ import sys
 from HsaNestedGenes import *
 
 
-# use this script to 
+# use this script to generate codon-based alignments of human-chimp orthologous coding sequences
 
-
-
-
-
-
-
-
+# usage python3 AlignCDSSequences.py
 
 
 # get GFF file
@@ -53,11 +47,11 @@ for gene in HumanGeneLongestTranscript:
     # get the corresponding sequence
     sequence = HumanCDS[transcript]
     HumanCodingSeq[gene] = sequence
-
-print(len(HumanCodingSeq))
 # remove genes with sequences not multiple of 3, lacking start codons, with internal stop codons
 HumanCodingSeq = FilerOutCDSSequences(HumanCodingSeq)
-print(len(HumanCodingSeq))
+# remove terminal stop codons
+HumanCodingSeq = RemoveTerminalStop(HumanCodingSeq)
+ 
 
 
 # get the coordinates of genes on each chromo
@@ -86,12 +80,11 @@ for gene in ChimpGeneLongestTranscript:
     # get the corresponding sequence
     sequence = ChimpCDS[transcript]
     ChimpCodingSeq[gene] = sequence
-
-print(len(ChimpCodingSeq))
 # remove genes with sequences not multiple of 3, lacking start codons, with internal stop codons
 ChimpCodingSeq = FilerOutCDSSequences(ChimpCodingSeq)
-print(len(ChimpCodingSeq))
-
+# remove terminal stop codons
+ChimpCodingSeq = RemoveTerminalStop(ChimpCodingSeq)
+ 
 
 # get the 1:1 orthologs between human-chimp-gorilla
 HsaPtrGgoOrthos = ParseOrthologFile('HumanChimpGorillaOrthologs.txt')
@@ -100,10 +93,59 @@ HumanChimpOrthos = {}
 for gene in HsaPtrGgoOrthos:
     HumanChimpOrthos[gene] = HsaPtrGgoOrthos[gene][0]
 
-total = 0
-# count the number of orthologs with sequences
-for gene in HumanChimpOrthos:
-    if gene in HumanCodingSeq and HumanChimpOrthos[gene] in ChimpCodingSeq:
-        total += 1
 
-print(total)
+# create a directory named pairs
+os.mkdir('./pairs/')
+
+
+# save orthologous sequences into separate files
+for gene in HumanChimpOrthos:
+    # check that both human and chimp genes have valid coding sequences
+    if gene in HumanCodingSeq and HumanChimpOrthos[gene] in ChimpCodingSeq:
+        # save human: chimp orthologous sequences in separate fasta files
+        # use human and chimp gene names in filename 
+        newfile = open('./pairs/' + gene + '_' + HumanChimpOrthos[gene] + '.tfa', 'w')
+        # write human gene and its sequence
+        newile.write('>' + gene + '\n')
+        newfile.write(HumanCodingSeq[gene] + '\n')
+        newfile.write('>' + HumanChimpOrthos[gene] + '\n')
+        newfile.write(ChimpCodingSeq[HumanChimpOrthos[gene]] + '\n')
+        newfile.close()
+        
+        
+# run to-coffee command lines to align protein sequences and get codon-based DNA alignments
+
+# make a list of fasta files with un-aligned orthologous sequences
+files = os.listdir('./pairs')
+
+# fasta files have a .tfa extension
+for filename in files:
+    # get a outputfile name for the protein translation
+    protein = filename[:-3] + '_p.tfa'
+    # translate dna sequence into protein sequence
+    dnaToPepCommand = "t_coffee -other_pg seq_reformat -in " + './pairs/' + filename + " -action +translate -output fasta_seq > " + protein
+    print(dnaToPepcommand)
+    os.system(dnaToPepcommand)
+    # when sequences have different length, t-coffee padds the short sequence with 'o'
+    # replace 'o' with '-'
+    os.system("sed -i 's/o/-/g '" + protein)
+
+    # align protein sequences
+    alignPepCommand = "t_coffee " + protein
+    print(alignPepCommand)
+    os.system(alignPepCommand)
+
+    # get name for aligned proteins
+    protein_ali = protein[:-3] + '.aln'
+    # get name for aligned DNA
+    DNA_ali = filename[:-3] +'.aln'
+    backtransCommand = 't_coffee -other_pg seq_reformat -in ' + './pairs/' + filename + ' -in2 ' + './pairs/' + protein_ali + ' -action +thread_dna_on_prot_aln' +\
+                       ' -output clustalw > ' + DNA_ali
+    print(backtransCommand)
+    os.system(backtransCommand)
+
+    # convert clustal format to fasta format
+    DNA_ali_fasta = filename[:-3] + '_aln.tfa'
+    convertDNAalignToFastaCommand = 't_coffee -other_pg seq_reformat -in' + './pairs'/ + DNA_ali + '-output fasta_aln > ' + DNA_ali_fasta
+    print(convertDNAalignToFastaCommand)
+    os.system(convertDNAalignToFastaCommand)
