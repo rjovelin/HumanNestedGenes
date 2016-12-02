57f3108255c7d96ff34d4490e063fe5a468a1d9f
Richard <richard.jovelin@oicr.on.ca>
2016-12-02 18:03:06 -0500

    plot proportions same and opposite strands for overlapping, contained and nested genes

--- PlotProportionsSameStrand.py
+++ PlotProportionsSameStrand.py
@@ -28,17 +28,28 @@ from HsaNestedGenes import *
 
 
 # load dictionaries of host and nested genes 
-# gene names have wormbase ID for cel and cbr, but transcript names for cr
 with open('HumanHostNestedGenes.json') as human_json_data:
     HumanHostGenes = json.load(human_json_data)
 with open('ChimpHostNestedGenes.json') as chimp_json_data:
     ChimpHostGenes = json.load(chimp_json_data)
 with open('GorillaHostNestedGenes.json') as gorilla_json_data:
     GorillaHostGenes = json.load(gorilla_json_data)
-with open('OrangOutanHostNestedGenes.json') as orangoutan_json_data:
-    OrangOutanHostGenes = json.load(orangoutan_json_data)
-with open('MacaqueHostNestedGenes.json') as macaque_json_data:
-    MacaqueHostGenes = json.load(macaque_json_data)
+# load dictionaries of host and contained genes
+with open('HumanContainedGenes.json') as human_json_data:
+    HumanContainedGenes = json.load(human_json_data)
+with open('ChimpContainedGenes.json') as chimp_json_data:
+    ChimpContainedGenes = json.load(chimp_json_data)
+with open('GorillaContainedGenes.json') as gorilla_json_data:
+    GorillaContainedGenes = json.load(gorilla_json_data)
+# load dictionaries with overlapping genes
+with open('HumanOverlappingGenes.json') as human_json_data:
+    HumanOverlappingGenes = json.load(human_json_data)
+with open('ChimpOverlappingGenes.json') as chimp_json_data:
+    ChimpOverlappingGenes = json.load(chimp_json_data)
+with open('GorillaOverlappingGenes.json') as gorilla_json_data:
+    GorillaOverlappingGenes = json.load(gorilla_json_data)
+
+
 
 # make lists of host-nested gene pairs
 HumanHostNestedPairs = GetHostNestedPairs(HumanHostGenes)
@@ -47,117 +58,171 @@ ChimpHostNestedPairs = GetHostNestedPairs(ChimpHostGenes)
 print('host-gene pairs in chimp', len(ChimpHostNestedPairs))
 GorillaHostNestedPairs = GetHostNestedPairs(GorillaHostGenes)
 print('host-gene pairs in gorilla', len(GorillaHostNestedPairs))
-OrangOutanHostNestedPairs = GetHostNestedPairs(OrangOutanHostGenes)
-print('host-gene pairs in orang-outan', len(OrangOutanHostNestedPairs))
-MacaqueHostNestedPairs = GetHostNestedPairs(MacaqueHostGenes)
-print('host-gene pairs in macaque', len(MacaqueHostNestedPairs))
+
+# make a list of host-contained gene pairs
+HumanHostContainedPairs = GetHostNestedPairs(HumanContainedGenes)
+print('host-contained pairs in human', len(HumanHostContainedPairs))
+ChimpHostContainedPairs = GetHostNestedPairs(ChimpContainedGenes)
+print('host-contained pairs in chimp', len(ChimpHostContainedPairs))
+GorillaHostContainedPairs = GetHostNestedPairs(GorillaContainedGenes)
+print('host-contained pairs in gorilla', len(GorillaHostContainedPairs))
+
+# make a list of overlapping gene pairs
+HumanOverlappingPairs = GetHostNestedPairs(HumanOverlappingGenes)
+print('human overlapping gene pairs', len(HumanOverlappingPairs))
+ChimpOverlappingPairs = GetHostNestedPairs(ChimpOverlappingGenes)
+print('chimp overlapping gene pairs', len(ChimpOverlappingPairs))
+GorillaOverlappingPairs = GetHostNestedPairs(GorillaOverlappingGenes)
+print('gorilla overlapping gene pairs', len(GorillaOverlappingPairs))
+
 
 # get GFF file
 HsaGFF = 'Homo_sapiens.GRCh38.86.gff3'
 PtrGFF = 'Pan_troglodytes.CHIMP2.1.4.86.gff3'
 GgoGFF = 'Gorilla_gorilla.gorGor3.1.86.gff3'
-PabGFF = 'Pongo_abelii.PPYG2.86.gff3'
-MmlGFF = 'Macaca_mulatta.Mmul_8.0.1.86.gff3'    
-    
     
 # make a list of primate GFF files
-GFFs = [HsaGFF, PtrGFF, GgoGFF, PabGFF, MmlGFF]
-
+GFFs = [HsaGFF, PtrGFF, GgoGFF]
 # make a list of host-nested pai lists
-Pairs = [HumanHostNestedPairs, ChimpHostNestedPairs, GorillaHostNestedPairs, OrangOutanHostNestedPairs, MacaqueHostNestedPairs]
+NestedPairs = [HumanHostNestedPairs, ChimpHostNestedPairs, GorillaHostNestedPairs]
+ContainedPairs = [HumanHostContainedPairs, ChimpHostContainedPairs, GorillaHostContainedPairs]
+OverlappingPairs = [HumanOverlappingPairs, ChimpOverlappingPairs, GorillaOverlappingPairs]
+
+
+# get gene coordinates in each species
+# get gene coordinates on each chromo
+HumanGeneChromoCoord = ChromoGenesCoord(GFFs[0])
+# map each gene to its mRNA transcripts
+HumanMapGeneTranscript = GeneToTranscripts(GFFs[0])
+# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
+HumanGeneChromoCoord = FilterOutGenesWithoutValidTranscript(HumanGeneChromoCoord, HumanMapGeneTranscript)
+# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
+HumanGeneCoord = FromChromoCoordToGeneCoord(HumanGeneChromoCoord)
+# get gene coordinates on each chromo
+ChimpGeneChromoCoord = ChromoGenesCoord(GFFs[1])
+# map each gene to its mRNA transcripts
+ChimpMapGeneTranscript = GeneToTranscripts(GFFs[1])
+# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
+ChimpGeneChromoCoord = FilterOutGenesWithoutValidTranscript(ChimpGeneChromoCoord, ChimpMapGeneTranscript)
+# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
+ChimpGeneCoord = FromChromoCoordToGeneCoord(ChimpGeneChromoCoord)
+# get gene coordinates on each chromo
+GorillaGeneChromoCoord = ChromoGenesCoord(GFFs[2])
+# map each gene to its mRNA transcripts
+GorillaMapGeneTranscript = GeneToTranscripts(GFFs[2])
+# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
+GorillaGeneChromoCoord = FilterOutGenesWithoutValidTranscript(GorillaGeneChromoCoord, GorillaMapGeneTranscript)
+# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
+GorillaGeneCoord = FromChromoCoordToGeneCoord(GorillaGeneChromoCoord)
+
+
+# make a list of gene coordinates for each species
+GeneCoordinates = [HumanGeneCoord, ChimpGeneCoord, GorillaGeneCoord]
 
 # create parallel lists of proportions for same strand and opposite strand 
 # for species ordered according to the GFF list
-SameStrand, OppositeStrand = [], []
+NestedSame, NestedOpposite = [], []
+ContainedSame, ContainedOpposite = [], []
+OverlappingSame, OverlappingOpposite = [], []
 
-# loop over GFF files, get the gene coordinates for the corresponding species
-for i in range(len(GFFs)):
-    print(GFFs[i][:GFFs[i].index('.')])
-    # get the coordinates of human genes on each chromo
-    # {chromo: {gene:[chromosome, start, end, sense]}}
-    SpGeneChromoCoord = ChromoGenesCoord(GFFs[i])
-    # map each gene to its mRNA transcripts
-    SpMapGeneTranscript = GeneToTranscripts(GFFs[i])
-    # remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
-    SpGeneChromoCoord = FilterOutGenesWithoutValidTranscript(SpGeneChromoCoord, SpMapGeneTranscript)
-    # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-    SpGeneCoord = FromChromoCoordToGeneCoord(SpGeneChromoCoord)
-    # count the number of same strand and opposite strand pairs
-    same, opposite = 0, 0
-    for pair in Pairs[i]:
-        orientation = set(GenePairOrientation(pair, SpGeneCoord))
-        if len(orientation) == 1:
-            same += 1
-        else:
-             assert len(orientation) == 2, 'there should be only 2 different signs'
-             opposite += 1
-    # add proportions to lists
-    SameStrand.append(same / (same + opposite))
-    OppositeStrand.append(opposite / (same + opposite))
+
+# populate lists [human, chimp, gorilla]
+for i in range(len(NestedPairs)):
+    NestedSame.append(GetSameOppositeStrandProportions(NestedPairs[i], GeneCoordinates[i])[0])
+    NestedOpposite.append(GetSameOppositeStrandProportions(NestedPairs[i], GeneCoordinates[i])[1])
+for i in range(len(ContainedPairs)):
+    ContainedSame.append(GetSameOppositeStrandProportions(ContainedPairs[i], GeneCoordinates[i])[0])
+    ContainedOpposite.append(GetSameOppositeStrandProportions(ContainedPairs[i], GeneCoordinates[i])[1])
+for i in range(len(OverlappingPairs)):
+    OverlappingSame.append(GetSameOppositeStrandProportions(OverlappingPairs[i], GeneCoordinates[i])[0])
+    OverlappingOpposite.append(GetSameOppositeStrandProportions(OverlappingPairs[i], GeneCoordinates[i])[1])
 
 
 # plot proportions for each species
 
-# make a list of species names
-SpeciesNames = ['Hsa', 'Ptr', 'Ggo', 'Pab', 'Mml']
-
 # create figure
 fig = plt.figure(1, figsize = (2.5, 2.8))
 
-# create subplot in figure
-# add a plot to figure (N row, N column, plot N)
-ax = fig.add_subplot(1, 1, 1)
-  
-# Create a horizontal bar plot for proportions of opposite strand pairs
-ax.bar([0, 0.4, 0.8, 1.2, 1.6], OppositeStrand, width = 0.3, label = 'opposite strand', color= '#fc8d59')
-# Create a horizontal bar plot for proportions of same strand pairs
-ax.bar([0, 0.4, 0.8, 1.2, 1.6], SameStrand, width = 0.3, bottom = OppositeStrand, label = 'same strand', color= '#91bfdb')
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, figure, OppositeStrand, SameStrand, XLabel, YAxis):
+    '''
+    (int, int, int, list, figure_object, str, int, list, list)
+    Take the number of a column, and rows in the figure object and the position of
+    the ax in figure, a list of data, a title, a maximum value for the Y axis,
+    a list with species names and list of X axis tick positions and return an
+    ax instance in the figure
+    '''    
+    # create subplot in figure
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    # Create a horizontal bar plot for proportions of opposite strand pairs
+    ax.bar([0, 0.4, 0.8], OppositeStrand, width = 0.3, label = 'opposite strand', color= '#fc8d59')
+    # Create a horizontal bar plot for proportions of same strand pairs
+    ax.bar([0, 0.4, 0.8], SameStrand, width = 0.3, bottom = OppositeStrand, label = 'same strand', color= '#91bfdb')
+    # set font for all text in figure
+    FigFont = {'fontname':'Arial'}   
+    # write label for y and x axis
+    if YAxis == True:
+        ax.set_ylabel('Proportion of gene pairs', color = 'black',  size = 8, ha = 'center', **FigFont)
+    ax.set_xlabel(XLabel, color = 'black',  size = 8, ha = 'center', **FigFont)
+    # write label for x axis
+    plt.xticks([0.15, 0.55, 0.95], ['Hsa', 'Ptr', 'Ggo'], ha = 'center', fontsize = 10, **FigFont)
+    # limit the y axis value range
+    plt.ylim([0, 1])   
+    # do not show lines around figure  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(True)    
+    ax.spines["right"].set_visible(False)    
+    if YAxis == True:
+        ax.spines["left"].set_visible(True)  
+    elif YAxis == False:
+        ax.spines["left"].set_visible(False)
+        
+    if YAxis == True:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='on',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'on',          
+            labelbottom='on', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 8,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+    elif YAxis == False:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='on',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='on', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 8,
+            labelleft = 'off',
+            direction = 'out') # ticks are outside the frame when bottom = 'on'      
+    
+    if YAxis == True:
+        # Set the tick labels font name
+        for label in ax.get_yticklabels():
+            label.set_fontname('Arial')   
+    # create a margin around the x axis
+    plt.margins(0.1)
+    return ax      
 
 
-# set font for all text in figure
-FigFont = {'fontname':'Arial'}   
-# write label for y axis
-ax.set_ylabel('Proportion of gene pairs', color = 'black', size = 10, ha = 'center', **FigFont)
-# write label for x axis
-plt.xticks([0.15, 0.55, 0.95, 1.35, 1.75], SpeciesNames, ha = 'center', fontsize = 10, **FigFont)
-        
-# limit the y axis value range
-plt.ylim([0, 1])   
-        
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(True)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(True)  
-# offset the spines
-for spine in ax.spines.values():
-    spine.set_position(('outward', 5))
-    
-# do not show ticks
-plt.tick_params(
-    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'on',          
-    labelbottom='on', # labels along the bottom edge are on
-    colors = 'black',
-    labelsize = 10,
-    direction = 'out') # ticks are outside the frame when bottom = 'on'  
-      
-# Set the tick labels font name
-for label in ax.get_yticklabels():
-    label.set_fontname('Arial')
-    
-# create a margin around the x axis
-plt.margins(0.05)
-    
-      
+ax1 = CreateAx(3, 1, 1, fig, OverlappingOpposite, OverlappingSame, 'Overlapping', False)
+ax2 = CreateAx(3, 1, 2, fig, ContainedOpposite, ContainedSame, 'Contained', False)
+ax3 = CreateAx(3, 1, 3, fig, NestedOpposite, NestedSame, 'Nested', True)
+
 # add legend
 S = mpatches.Patch(facecolor = '#91bfdb' , edgecolor = 'black', linewidth = 1, label= 'same')
 O = mpatches.Patch(facecolor = '#fc8d59' , edgecolor = 'black', linewidth = 1, label= 'opposite')
-ax.legend(handles = [S, O], loc = (-0.1, 1), fontsize = 8, frameon = False, ncol = 2)
+ax1.legend(handles = [S, O], loc = (0.1, 1), fontsize = 8, frameon = False, ncol = 2)
 
 # make sure subplots do not overlap
 plt.tight_layout()
