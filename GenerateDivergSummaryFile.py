ac88cf08e7ac1b2aefa63b06553e51c524d8e33a
Richard <richard.jovelin@oicr.on.ca>
2016-12-05 15:00:18 -0500

    filter out genes with extreme dN and dS

---
+++ GenerateDivergSummaryFile.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 14:43:46 2016
+
+@author: RJovelin
+"""
+
+# use this script to write summary of nucleotide divergence to file 
+
+import os
+
+# make a list of codeml output files in current directory
+outfiles = [i for i in os.listdir('./HumanChimpCodeml/') if '.out.txt' in i]
+
+# save summary file in parent directory
+newfile = open('./HumanChimpSeqDiverg.txt', 'w')
+# write header
+newfile.write('\t'.join(['Human_Gene', 'Chimp_Gene', 'dN', 'dS', 'dN/dS']) + '\n')
+
+# loop over outputfiles
+for filename in outfiles:
+    infile = open('./HumanChimpCodeml/' + filename, 'r')
+    # get human gene name
+    HumanGene = filename[:filename.index('_')]
+    # get chimp gene name
+    ChimpGene = filename[filename.index('_')+1: filename.index('.out')]
+    # loop through file    
+    for line in infile:
+        if 'tree length for dN' in line:
+            line = line.split()
+            dN = float(line[-1])
+        elif 'tree length for dS' in line:
+            line = line.split()
+            dS = float(line[-1])
+    if dS != 0:
+        omega = dN / dS
+    elif dS == 0:
+        omega = 'NA'
+    # filter out genes with dN or dS values > 10
+    if dN <= 10 and dS <= 10:
+        # write results to file    
+        newfile.write('\t'.join([HumanGene, ChimpGene, str(dN), str(dS), str(omega)]) + '\n')
+        # close outputfile
+    infile.close()
+# close summary file
+newfile.close()
+print('done writing summary file')
+
