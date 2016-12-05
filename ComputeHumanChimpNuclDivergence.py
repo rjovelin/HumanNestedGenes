017ca318a7633b497d8bdd45ab6020c4747c495e
Richard <richard.jovelin@oicr.on.ca>
2016-12-05 14:58:35 -0500

    bring code to generate summary file to a separate script

--- ComputeHumanChimpNuclDivergence.py
+++ ComputeHumanChimpNuclDivergence.py
@@ -91,43 +91,5 @@ for filename in ctlfiles:
     os.system('codeml ' + filename)
 print('done running codeml')
 
-# write summary of nucleotide divergence to file 
-
-# make a list of codeml output files in current directory
-outfiles = [i for i in os.listdir('./') if '.out.txt' in i]
-
-# save summary file in parent directory
-newfile = open('../HumanChimpSeqDiverg.txt', 'w')
-# write header
-newfile.write('\t'.join(['Human_Gene', 'Chimp_Gene', 'dN', 'dS', 'dN/dS']) + '\n')
-
-# loop over outputfiles
-for filename in outfiles:
-    infile = open(filename, 'r')
-    # get human gene name
-    HumanGene = filename[:filename.index('_')]
-    # get chimp gene name
-    ChimpGene = filename[filename.index('_')+1: filename.index('.out')]
-    # loop through file    
-    for line in infile:
-        if 'tree length for dN' in line:
-            line = line.split()
-            dN = float(line[-1])
-        elif 'tree length for dS' in line:
-            line = line.split()
-            dS = float(line[-1])
-    if dS != 0:
-        omega = dN / dS
-    elif dS == 0:
-        omega = 'NA'
-    # write results to file    
-    newfile.write('\t'.join([HumanGene, ChimpGene, str(dN), str(dS), str(omega)]) + '\n')
-    # close outputfile
-    infile.close()
-# close summary file
-newfile.close()
-print('done writing summary file')
-
 # go back to parent directory
 os.chdir('../')
-
