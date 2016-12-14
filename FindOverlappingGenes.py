aa10570c404707ff47e977cacf3b9f319974d670
Richard <richard.jovelin@oicr.on.ca>
2016-12-14 16:11:01 -0500

    identify overlapping genes based on gene coordinates

--- FindOverlappingGenes.py
+++ FindOverlappingGenes.py
@@ -5,11 +5,10 @@ Created on Fri Nov 11 16:25:38 2016
 @author: RJovelin
 """
 
-# use this script to identify overlapping genes, nested and itnronic nested genes
-# in human, chimp and gorilla
-# save dictionaries of overlapping and hosy:nested genes as json files
+# use this script to identify overlapping genes in human, chimp and gorilla
+# save dictionaries of overlapping genes as json files
 
-# usage python3 FindNestedGenes.py
+# usage python3 FindOverlappingGenes.py
 
 
 import os
@@ -51,64 +50,18 @@ for i in range(len(GFFs)):
     # Find overlapping genes {gene1: [gene2, gene3]}
     SpOverlappingGenes = FindOverlappingGenePairs(SpGeneChromoCoord, SpOrderedGenes)
     print('found overlapping genes', len(SpOverlappingGenes))
-    # Find genes fully contained in another gene {containing: [contained1, contained2]}
-    SpContainedGenes = FindContainedGenePairs(SpGeneCoord, SpOverlappingGenes)
-    print('found genes contained in other genes', len(SpContainedGenes))
-    # Map Transcript names to gene names {transcript: gene}
-    SpMapTranscriptGene = TranscriptToGene(GFFs[i])
-    print('mapped transcripts to their parent gene', len(SpMapTranscriptGene))
-    # get the exon coordinates of all transcript {transcript: [[exon_start, exon_end]]}
-    SpExonCoord = GeneExonCoord(GFFs[i])
-    print('got exon coordinates', len(SpExonCoord))
-    SpExonCoord = CleanGeneFeatureCoord(SpExonCoord, SpMapTranscriptGene)
-    print('cleaned up exon coordinates of non-mRNA transcripts', len(SpExonCoord))
-    # get the intron coordinates of all transcripts {transcript: [[intron_start, intron_end]]}
-    SpIntronCoord = GeneIntronCoord(SpExonCoord)
-    print('got intron coordinates', len(SpIntronCoord))
-    SpIntronCoord = CleanGeneFeatureCoord(SpIntronCoord, SpMapTranscriptGene)
-    print('cleaned up intron coordinates of non-mRNA transcripts', len(SpIntronCoord))
-    # Combine all intron from all transcripts for a given gene {gene: [(region_start, region_end), ...]}
-    SpCombinedIntronCoord = CombineAllGeneRegions(SpIntronCoord, SpMapTranscriptGene)
-    print('combined introns for each gene', len(SpCombinedIntronCoord))
-    # identify itnronic nested genes {host_gene: [intronic_nested_gene]}
-    SpHostGenes = FindIntronicNestedGenePairs(SpContainedGenes, SpCombinedIntronCoord, SpGeneCoord)
-    print('identified intronic nested genes', len(SpHostGenes))
-
-    
+    # save dictionaries as json file
     if i == 0:
-        # save contained genes as json file
-        newfile = open('HumanContainedGenes.json', 'w')
-        json.dump(SpContainedGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
-        # save intronic nested genes as json file
-        newfile = open('HumanHostNestedGenes.json', 'w')
-        json.dump(SpHostGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
         # save overlapping genes as json file
         newfile = open('HumanOverlappingGenes.json', 'w')
         json.dump(SpOverlappingGenes, newfile, sort_keys = True, indent = 4)
         newfile.close()
     elif i == 1:
-        # save contained genes as json file
-        newfile = open('ChimpContainedGenes.json', 'w')
-        json.dump(SpContainedGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
-        # save intronic nested genes as json file
-        newfile = open('ChimpHostNestedGenes.json', 'w')
-        json.dump(SpHostGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
         # save overlapping genes as json file
         newfile = open('ChimpOverlappingGenes.json', 'w')
         json.dump(SpOverlappingGenes, newfile, sort_keys = True, indent = 4)
         newfile.close()
     elif i == 2:
-        newfile = open('GorillaContainedGenes.json', 'w')
-        json.dump(SpContainedGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
-        # save intronic nested genes as json file
-        newfile = open('GorillaHostNestedGenes.json', 'w')
-        json.dump(SpHostGenes, newfile, sort_keys = True, indent = 4)
-        newfile.close()
         # save overlapping genes as json file
         newfile = open('GorillaOverlappingGenes.json', 'w')
         json.dump(SpOverlappingGenes, newfile, sort_keys = True, indent = 4)
