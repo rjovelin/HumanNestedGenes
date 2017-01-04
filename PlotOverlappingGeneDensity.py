00d5ee0e0bbea627315398cef1e80bb4702760db
Richard <richard.jovelin@oicr.on.ca>
2017-01-04 18:55:29 -0500

    plot gene densities on each chromo

--- PlotOverlappingGeneDensity.py
+++ PlotOverlappingGeneDensity.py
@@ -33,23 +33,6 @@ from HsaNestedGenes import *
 json_data = open('HumanOverlappingGenes.json')
 Overlapping = json.load(json_data)
 json_data.close()
-# load dictionary of nested gene pairs
-json_data = open('HumanNestedGenes.json')
-Nested = json.load(json_data)
-json_data.close()
-# load dictionary of pibbyback gene pairs
-json_data = open('HumanPiggyBackGenes.json')
-Piggyback = json.load(json_data)
-json_data.close()
-# load dictionary of convergent gene pairs
-json_data = open('HumanConvergentGenes.json')
-Convergent = json.load(json_data)
-json_data.close()
-# load dictionary of divergent gene pairs
-json_data = open('HumanDivergentGenes.json')
-Divergent = json.load(json_data)
-json_data.close()
-
 
 # get GFF file
 GFF = 'Homo_sapiens.GRCh38.86.gff3'
@@ -67,18 +50,9 @@ OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
 
 # make sets of genes
 OverlapGenes = MakeFullPartialOverlapGeneSet(Overlapping)
-NestedGenes = MakeFullPartialOverlapGeneSet(Nested)
-PiggybackGenes = MakeFullPartialOverlapGeneSet(Piggyback)
-ConvergentGenes = MakeFullPartialOverlapGeneSet(Convergent)
-DivergentGenes = MakeFullPartialOverlapGeneSet(Divergent)
 
 # create lists of gene pairs
 OverlappingPairs = GetHostNestedPairs(Overlapping)
-NestedPairs = GetHostNestedPairs(Nested)
-PiggybackPairs = GetHostNestedPairs(Piggyback)
-ConvergentPairs = GetHostNestedPairs(Convergent)
-DivergentPairs = GetHostNestedPairs(Divergent)
-
 
 # get the gene counts on each chromo
 GeneCounts = {}
@@ -112,214 +86,183 @@ for i in range(len(content)):
     chromo = content[i][0].split()[0]
     dna = ''.join(content[i][1:])
     Genome[chromo] = dna
-    
-    
-   
-for chromo in Freq:
-    if Freq[chromo] > 1:
-        print(chromo, Freq[chromo], str(len(Genome[chromo])) + ' bp', str(len(Genome[chromo]) // 1000000) + ' Mb', str(len(Genome[chromo]) // 100000) + ' per 100Kb window')
 
-
-
-# make a list of chromosomes with overlapping genes
-Chromosomes = set()
-for pair in OverlappingPairs:
-    Chromosomes.add(GeneCoord[pair[0]][0])
-Chromosomes = list(Chromosomes)    
+  
+# set up minimum overlapping gene frequency (in %)
+MinimumFreq = 1
+# consider chromosomes with frequency of overlapping genes > minimum frequency
+Chromosomes = [chromo for chromo in Freq if Freq[chromo] > MinimumFreq]
 Chromosomes.sort()
 
-# look for clusters o overlapping genes on each chromo
-
-# get the first position overlapping between pairs of genes
+# look for clusters of overlapping genes on each chromo
 # create a dict {chromo: [positions]}
 OverlapStart = {}
-for pair in OverlappingPairs:
-    chromo = GeneCoord[pair[0]][0]
-    coord1 = set(range(GeneCoord[pair[0]][1], GeneCoord[pair[0]][2]))
-    coord2 = set(range(GeneCoord[pair[1]][1], GeneCoord[pair[1]][2]))
-    common = list(coord1.intersection(coord2))
-    common.sort()
-    start = common[0]
+for gene in OverlapGenes:
+    chromo = GeneCoord[gene][0]
+    if GeneCoord[gene][-1] == '+':
+        start = GeneCoord[gene][1]
+    elif GeneCoord[gene][-1] == '-':
+        start = GeneCoord[gene][2] - 1
     if chromo in OverlapStart:
         OverlapStart[chromo].append(start)
     else:
         OverlapStart[chromo] = [start]
-
 # sort positions
 for chromo in OverlapStart:
     OverlapStart[chromo].sort()
 
+# get the start position of each gene on chromo
+GeneStart = {}
+for gene in GeneCoord:
+    chromo = GeneCoord[gene][0]
+    # only consider chromosomes with enough overlapping genes
+    if chromo in Chromosomes:
+        # check orientation
+        if GeneCoord[gene][-1] == '+':
+            start = GeneCoord[gene][1]
+        elif GeneCoord[gene][-1] == '-':
+            start = GeneCoord[gene][2] - 1
+        if chromo in GeneStart:
+            GeneStart[chromo].append(start)
+        else:
+            GeneStart[chromo] = [start]
+# sort positions
+for chromo in GeneStart:
+    GeneStart[chromo].sort()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-######## continue here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+# set up interval length in bp
+Interval = int(10000000 / 2)
 
 
    
-## create a function to count the number of chemo per window interval
-#def CountChemoWindow(chemo_start, genome, chromo, window):
-#    '''
-#    (dict, str, int) -> list
-#    Take the dictionary with chemo gene start positions per chromo, 
-#    the dict with genome sequence, a chromosome and a window interval in bp
-#    and return a list with the number of chemo genes on chromo per window interval'
-#    '''
-#    # create list with count of gene o repeat in 100000 bp windows
-#    range_counts = [0] * (len(genome[chromo]) // window)
-#    for start in chemo_start[chromo]:
-#        which_range = start // window
-#        if which_range == len(range_counts):
-#            which_range -= 1
-#        # count repeats
-#        range_counts[which_range] += 1
-#    return range_counts
-#    
-#    
-## set up interval length in bp
-#Interval = 100000
-#print('Interval window: {0} bp'.format(Interval))    
-#
-## get the count of chemo gene per window
-#WindowCount = {}
-#for chromo in chemo_start:
-#    range_counts = CountChemoWindow(chemo_start, genome, chromo, Interval)     
-#    WindowCount[chromo] = range_counts
-#print('got chemo count per window')    
-#
-## create a list with the position of each window interval
-#Positions = {}
-#for chromo in WindowCount:
-#    pos = [i for i in range(len(WindowCount[chromo]))] 
-#    Positions[chromo] = pos     
-#    print('position', chromo, len(pos))
-#    print('interval length', len(genome[chromo]) // Interval)
-#print('got positions of window intervals')
-#
-#
-## create figure
-#fig = plt.figure(1, figsize = (4, 1))
-## add a plot to figure (1 row, 1 column, 1 plot)
-#ax = fig.add_subplot(1, 1, 1)  
-#
-## find the longtest chromo
-#chromoLength = [[len(genome[chromo]), chromo] for chromo in HighFreq]
-#chromoLength.sort()
-#size = [chromoLength[i][0] for i in range(len(chromoLength))]
-#LG = [chromoLength[i][1] for i in range(len(chromoLength))]
-#longest, maxlength = chromoLength[-1][-1], chromoLength[-1][0] 
-#
-## create a list of colors
-##colorscheme = ['#a6cee3','#1f78b4','#b2df8a','#33a02c']
-#colorscheme = ['#1b9e77','#d95f02','#7570b3']
-#
-#Graph = {}
-## loop over chromo, from chromo with lowest to highest count
-#for i in range(len(LG)):
-#    print(LG[i])
-#    # plot the repeat of gene density per window
-#    graph = ax.plot(Positions[LG[i]], WindowCount[LG[i]], linewidth = 1.2, color = colorscheme[i], alpha = 0.7)
-#    Graph[LG[i]] = graph
-#    
-#ax.set_ylabel('GPCRs /100 Kb', size = 10, ha = 'center', fontname = 'Arial')
-# 
-## set x axis label
-#ax.set_xlabel('Position along linkage group (Mb)', size = 10, ha = 'center', fontname = 'Arial')
-#
-## do not show lines around figure, keep bottow line  
-#ax.spines["top"].set_visible(False)    
-#ax.spines["bottom"].set_visible(True)    
-#ax.spines["right"].set_visible(False)    
-#ax.spines["left"].set_visible(False)      
-## offset the spines
-#for spine in ax.spines.values():
-#  spine.set_position(('outward', 5))
-#  
-## add a light grey horizontal grid to the plot, semi-transparent, 
-#ax.yaxis.grid(True, linestyle='--', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
-## hide these grids behind plot objects
-#ax.set_axisbelow(True)
-#
-## do not show ticks on 1st graph
-#ax.tick_params(
-#    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
-#    which='both',      # both major and minor ticks are affected
-#    bottom='on',      # ticks along the bottom edge are off
-#    top='off',         # ticks along the top edge are off
-#    right = 'off',
-#    left = 'off',          
-#    labelbottom='on', # labels along the bottom edge are off 
-#    colors = 'black',
-#    labelsize = 10,
-#    direction = 'out') # ticks are outside the frame when bottom = 'on
-#
-## do not show ticks
-#ax.tick_params(
-#    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
-#    which='both',      # both major and minor ticks are affected
-#    bottom='off',      # ticks along the bottom edge are off
-#    top='off',         # ticks along the top edge are off
-#    right = 'off',
-#    left = 'off',          
-#    labelbottom='off', # labels along the bottom edge are off 
-#    colors = 'black',
-#    labelsize = 10,
-#    direction = 'out') # ticks are outside the frame when bottom = 'on
-#
-#print('longest chromo', longest, maxlength)
-#
-## determine tick position on x axis
-#xpos =  [j for j in range(0, len(Positions[longest]), 10)]
-## convert interval windows numbers to genomic positions
-#xtext = list(map(lambda x : (x * Interval) / 1000000, xpos))
-#Xtext = []
-#for i in xtext:
-#    if i % 2 == 0:
-#        Xtext.append(str(int(i)))
-#    else:
-#        Xtext.append('')
-#
-## set up tick positions and labels
-#plt.xticks(xpos, Xtext, rotation = 0, fontsize = 10, fontname = 'Arial')
-#
-## add lines
-#lns = Graph[LG[0]]
-#for chromo in LG[1:]:
-#    lns += Graph[chromo]
-## get labels
-#labs = []
-#for chromo in LG:
-#    assert chromo.count('_') == 2
-#    lg = chromo[chromo.index('_', chromo.index('_')+1, -1)+1:]
-#    labs.append('LG' + lg)
-## plot legend
-#ax.legend(lns, labs, loc=1, fontsize = 8, frameon = False)
-#
-#fig.savefig('truc.pdf', bbox_inches = 'tight')
+# get the count of genes per window
+GeneWindowCount = {}
+for chromo in Chromosomes:
+    counts, binedges = np.histogram(GeneStart[chromo], bins = range(0, len(Genome[chromo]), Interval))
+    GeneWindowCount[chromo] = [list(counts), list(binedges)]
+# get the count of overlaps per window
+OverlapWindowCount = {}
+for chromo in Chromosomes:
+    counts, binedges = np.histogram(OverlapStart[chromo], bins = range(0, len(Genome[chromo]), Interval))
+    OverlapWindowCount[chromo] = [list(counts), list(binedges)]
+
+# add 0 to match dimensions between bin edges and counts
+for chromo in Chromosomes:
+    GeneWindowCount[chromo][0].insert(0,0)
+    OverlapWindowCount[chromo][0].insert(0,0) 
+
+# convert counts to frequencies by dividing each count in window by number of genes on chromosome
+for chromo in Chromosomes:
+    for i in range(len(GeneWindowCount[chromo][0])):
+        GeneWindowCount[chromo][0][i] = GeneWindowCount[chromo][0][i] / GeneCounts[chromo]
+    for i in range(len(OverlapWindowCount[chromo][0])):
+        OverlapWindowCount[chromo][0][i] = OverlapWindowCount[chromo][0][i] / OverlapCounts[chromo]
+
+
+# get maximum frequency
+Maximum = 0
+for chromo in GeneWindowCount:
+    for i in GeneWindowCount[chromo][0]:
+        if i >= Maximum:
+            Maximum = i
+    for i in OverlapWindowCount[chromo][0]:
+        if i >= Maximum:
+            Maximum = i
+
+
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, figure, GeneWindowCount, OverlapWindowCount, chromo, YMax, YLabel):
+    '''
+    (int, int, int, figure_object, dict, dict, str, float, bool)
+    Take the number of a column, and rows in the figure object and the position of
+    the ax in figure, the dictionaries with gene densities on each chromosomes
+    for all genes and for overlapping genes only, the chromosome of interest,
+    the maximum frequency value, and a boolean indicating whether the Y axis should
+    be represented and return a subplot in figure
+    '''    
+    # create subplot in figure
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    # plot gene density per window
+    ax.plot(GeneWindowCount[chromo][1], GeneWindowCount[chromo][0], linewidth = 1, linestyle = '-', color = 'black', alpha = 1)
+    ax.plot(OverlapWindowCount[chromo][1], OverlapWindowCount[chromo][0], linewidth = 1, linestyle = '-', color = 'grey', alpha = 1)
+       
+    # set font for all text in figure
+    FigFont = {'fontname':'Arial'}   
+    # set axis labels
+    if YLabel == True:
+        ax.set_ylabel('Gene density', size = 10, ha = 'center', **FigFont)
+    ax.set_xlabel(chromo, size = 10, color = 'black', ha = 'center', **FigFont )        
+        
+    # add a range for the Y axis
+    plt.ylim([0, YMax])
+    
+    # do not show lines around figure  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(True)    
+    ax.spines["right"].set_visible(False) 
+    if YLabel == True:
+        ax.spines["left"].set_visible(True)  
+    elif YLabel == False:
+        ax.spines["left"].set_visible(False)
+    
+    if YLabel == True:
+        # edit tick paramters
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'on',          
+            labelbottom='off', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 10,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+           
+    elif YLabel == False:
+        # edit tick paramters
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='off', # labels along the bottom edge are on
+            labelleft = 'off',            
+            colors = 'black',
+            labelsize = 8,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+           
+    # set x axis ticks
+    # plt.xticks(GeneWindowCount[chromo][1], list(map(lambda x: str(x), GeneWindowCount[chromo][1])), rotation = 0, fontsize = 8, fontname = 'Arial')
+    plt.xticks([], [])
+    
+    if YLabel == True:
+        # Set the tick labels font name
+        for label in ax.get_yticklabels():
+            label.set_fontname('Arial')   
+    # create a margin around the x axis
+    #plt.margins(0.1)
+    return ax      
+
+
+# create figure
+fig = plt.figure(1, figsize = (10, 5))
+
+j = 1
+LG = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '19', '20', '21', '22', 'X']
+for i in range(len(LG)):
+    if i == 0 or i == 11:
+        YLabel = True
+    else:
+        YLabel = False
+    ax = CreateAx(11, 2, j, fig, GeneWindowCount, OverlapWindowCount, LG[i], Maximum, YLabel)
+    j += 1
+
+# make sure subplots do not overlap
+plt.tight_layout()
+
+fig.savefig('truc.pdf', bbox_inches = 'tight')
