a1a408217f16a721a467a5b9af0c8de139f8cb47
Richard <richard.jovelin@oicr.on.ca>
2017-01-09 12:57:13 -0500

    tested enrichement of intronless internal genes on same strand

--- EnrichmentIntronlessNestedTable.py
+++ EnrichmentIntronlessNestedTable.py
@@ -40,86 +40,90 @@ with open('HumanNestedGenes.json') as human_json_data:
 # get GFF file
 GFF = 'Homo_sapiens.GRCh38.86.gff3'
  
+# count internal (nested) intronless genes, genes with introns that are on the
+# same strand and opposite strand relative to their hosts
+NoIntronSame, NoIntronOpposite, WithIntronSame, WithIntronOpposite = 0, 0, 0, 0
 
-# create lists to store the number of intronless nested genes and nested genes
-# with introns that are on the same strand and opposite strand with their hosts
-# [human, chimp, gorilla, orang-outan, macaque]
-IntronlessSame, IntronlessOpposite, IntronSame, IntronOpposite = [], [], [], []
+# find nested and intronic-nested genes 
+# get the coordinates of genes on each chromo
+# {chromo: {gene:[chromosome, start, end, sense]}}
+GeneChromoCoord = ChromoGenesCoord(GFF)
+# map each gene to its mRNA transcripts
+MapGeneTranscript = GeneToTranscripts(GFF)
+# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
+GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
+# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
+GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
+# Map Transcript names to gene names {transcript: gene}
+MapTranscriptGene = TranscriptToGene(GFF)
+# get the coordinates of all exons    
+ExonCoord = GeneExonCoord(GFF)
+ExonCoord = CleanGeneFeatureCoord(ExonCoord, MapTranscriptGene)
+# get the intron coordinates of all transcripts {transcript: [[intron_start, intron_end]]}
+IntronCoord = GeneIntronCoord(ExonCoord)
+IntronCoord = CleanGeneFeatureCoord(IntronCoord, MapTranscriptGene)
+# get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
+TranscriptCoordinates = TranscriptsCoord(GFF)
+# map genes to their longest transcript {gene: longest_transcript}
+GeneLongestTranscript = LongestTranscript(TranscriptCoordinates, MapGeneTranscript)
+# match longest transcript of the nested genes to transcript of the host gene (longest transcript in priority)
+Matches = MatchHostTranscriptWithNestedTranscript(Nested, MapGeneTranscript, GeneLongestTranscript, TranscriptCoordinates, IntronCoord)
+# list all host, nested transcript pairs [[host, nested]]
+HostNestedPairs = GetHostNestedPairs(Matches)
+
+# loop over host-nested transcript pairs
+for i in range(len(HostNestedPairs)):
+    # check that both transcripts have coordinates and have corresponding gene names    
+    assert HostNestedPairs[i][0] in MapTranscriptGene
+    assert HostNestedPairs[i][0] in TranscriptCoordinates    
+    assert HostNestedPairs[i][1] in MapTranscriptGene
+    assert HostNestedPairs[i][1] in TranscriptCoordinates    
+    # get the orientation of each transcript [+,+]
+    OrientationPair = GenePairOrientation(HostNestedPairs[i], TranscriptCoordinates)
+    # determine if the nested transcript has introns
+    if HostNestedPairs[i][1] in IntronCoord:
+        IntronLess = False
+    else:
+        IntronLess = True
+    # check presence of intron and orientation of host and nested transcripts
+    if IntronLess == True and len(set(OrientationPair)) == 1:
+        # intronless, same orientation
+        NoIntronSame += 1
+        assert set(OrientationPair) == {'-', '-'} or set(OrientationPair) == {'+', '+'}
+    elif IntronLess == True and len(set(OrientationPair)) == 2:
+        # intronless, differente orientation
+        NoIntronOpposite += 1
+        assert set(OrientationPair) == {'-', '+'} 
+    elif IntronLess == False and len(set(OrientationPair)) == 1:
+        # with introns and same orientation
+        WithIntronSame += 1
+        assert set(OrientationPair) == {'-', '-'} or set(OrientationPair) == {'+', '+'}
+    elif IntronLess == False and len(set(OrientationPair)) == 2:
+        # with introns and opposite orientation
+        WithIntronOpposite += 1
+        assert set(OrientationPair) == {'-', '+'}
 
 
-# loop over GFF files, find nested and intronic=nested genes in each species 
-for i in range(len(GFFs)):
-    print(GFFs[i][:GFFs[i].index('.')], SpeciesNames[i])
-    # get the coordinates of genes on each chromo
-    # {chromo: {gene:[chromosome, start, end, sense]}}
-    SpGeneChromoCoord = ChromoGenesCoord(GFFs[i])
-    # map each gene to its mRNA transcripts
-    SpMapGeneTranscript = GeneToTranscripts(GFFs[i])
-    # remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
-    SpGeneChromoCoord = FilterOutGenesWithoutValidTranscript(SpGeneChromoCoord, SpMapGeneTranscript)
-    # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-    SpGeneCoord = FromChromoCoordToGeneCoord(SpGeneChromoCoord)
-    # Map Transcript names to gene names {transcript: gene}
-    SpMapTranscriptGene = TranscriptToGene(GFFs[i])
-    # get the coordinates of all exons    
-    SpExonCoord = GeneExonCoord(GFFs[i])
-    SpExonCoord = CleanGeneFeatureCoord(SpExonCoord, SpMapTranscriptGene)
-    # get the intron coordinates of all transcripts {transcript: [[intron_start, intron_end]]}
-    SpIntronCoord = GeneIntronCoord(SpExonCoord)
-    SpIntronCoord = CleanGeneFeatureCoord(SpIntronCoord, SpMapTranscriptGene)
-    # get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
-    SpTranscriptCoordinates = TranscriptsCoord(GFFs[i])
-    # map genes to their longest transcript {gene: longest_transcript}
-    SpGeneLongestTranscript = LongestTranscript(SpTranscriptCoordinates, SpMapGeneTranscript)
-    # match longest transcript of the nested genes to transcript of the host gene (longest transcript in priority)
-    SpMatches = MatchHostTranscriptWithNestedTranscript(HostGenes[i], SpMapGeneTranscript, SpGeneLongestTranscript, SpTranscriptCoordinates, SpIntronCoord)
-    # list all host, nested transcript pairs [[host, nested]]
-    SpHostNestedPairs = GetHostNestedPairs(SpMatches)
-    # count nested intronless-same, intronless-opposite, intron-same, intron-opposite
-    NoIntronSame, NoIntronOpposite, WithIntronSame, WithIntronOpposite = 0, 0, 0, 0
-    # loop over host-nested transcript pairs
-    for i in range(len(SpHostNestedPairs)):
-        # get the orientation of each transcript [+,+]
-        OrientationPair = GenePairOrientation(SpHostNestedPairs[i], SpTranscriptCoordinates)
-        # determine if the nested transcript has introns
-        if SpHostNestedPairs[i][1] in SpIntronCoord:
-            IntronLess = False
-        else:
-            IntronLess = True
-        # check presence of intron and orientation of host and nested transcripts
-        if IntronLess == True and len(set(OrientationPair)) == 1:
-            # intronless, same orientation
-            NoIntronSame += 1
-        elif IntronLess == True and len(set(OrientationPair)) == 2:
-            # intronless, differente orientation
-            NoIntronOpposite += 1
-        elif IntronLess == False and len(set(OrientationPair)) == 1:
-            # with introns and same orientation
-            WithIntronSame += 1
-        elif IntronLess == False and len(set(OrientationPair)) == 2:
-            # with introns and opposite orientation
-            WithIntronOpposite += 1
-    # populate lists
-    IntronlessSame.append(NoIntronSame)
-    IntronlessOpposite.append(NoIntronOpposite)
-    IntronSame.append(WithIntronSame)
-    IntronOpposite.append(WithIntronOpposite)      
+# test that the proportions of intron-less and with-intron genes are the same on both strands
+P = stats.fisher_exact([[NoIntronSame, NoIntronOpposite], [WithIntronSame, WithIntronOpposite]])[1]
 
-# create a list to store the P values, in the same order as the lists with number of nested genes
-PVal = []
-# perform statistical tests to find out if there is an over-representation of intronless genes in same orientation
-for i in range(len(SpeciesNames)):
-    P = stats.fisher_exact([[IntronlessSame[i], IntronlessOpposite[i]], [IntronSame[i], IntronOpposite[i]]])[1]
-    PVal.append(P)
-
-# create table
-# species, intronless-same, intronless-opposite, intron-same, intron-opposite, Pval
+# open file for writing results
 newfile = open('ContingencyTableIntronlessGenes.txt', 'w')
-header = '\t'.join(['Species', 'Intronless-Same', 'Intronless-Opposite', 'Intron-Same', 'Intron-Opposite', 'P'])
-newfile.write(header + '\n')
 
-# loop over species:
-for i in range(len(SpeciesNames)):
-    line = '\t'.join([SpeciesNames[i], str(IntronlessSame[i]), str(IntronlessOpposite[i]), str(IntronSame[i]), str(IntronOpposite[i]), str(PVal[i])])
-    newfile.write(line + '\n')
+
+
+# create contingency table table
+newfile.write('Table 1. Number of internal (nested genes) with and without introns and their orientation\n')
+newfile.write('\t'.join(['', 'Intronless', 'WithIntron', 'Ratio Intronless/Total', 'P']) + '\n')
+newfile.write('\t'.join(['Same', str(NoIntronSame), str(WithIntronSame), str(round(NoIntronSame / (NoIntronSame + WithIntronSame), 4)), str(P)]) + '\n')
+newfile.write('\t'.join(['Opposite', str(NoIntronOpposite), str(WithIntronOpposite), str(round(NoIntronOpposite / (NoIntronOpposite + WithIntronOpposite), 4)), str(P)]) + '\n')
+
+
 newfile.close()
+
+
+## make a set of un-nested genes
+#UnNestedGenes = MakeNonOverlappingGeneSet(HostGenes, GeneCoord)
+
+
+
