12b0a2795fe7b31b3ff828baa824948084485d38
Richard <richard.jovelin@oicr.on.ca>
2017-01-10 17:28:34 -0500

    edited line width and colors

--- PlotHighestExpression.py
+++ PlotHighestExpression.py
@@ -18,6 +18,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib import rc
+import matplotlib.gridspec as gridspec
 rc('mathtext', default='regular')
 import json
 import random
@@ -81,24 +82,6 @@ PiggyBackGenes = MakeFullPartialOverlapGeneSet(Piggyback)
 # make a set of non-overlapping genes
 NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlapping, GeneCoord)
 
-# create lists of nested gene pairs with same and opposite directions
-same, opposite = [], []
-for pair in NestedPairs:
-    orientation = GenePairOrientation(pair, GeneCoord)
-    if len(set(orientation)) == 2:
-        opposite.append(pair)
-    elif len(set(orientation)) == 1:
-        same.append(pair)
-# create sets of internal and external nested genes depending on orientation 
-InternalSameGenes, InternalOppositeGenes, ExternalSameGenes, ExternalOppositeGenes = set(), set(), set(), set()
-for pair in same:
-    ExternalSameGenes.add(pair[0])
-    InternalSameGenes.add(pair[1])
-for pair in opposite:
-    ExternalOppositeGenes.add(pair[0])
-    InternalOppositeGenes.add(pair[1])
-
-
 # parse the GTEX expression summary file to obtain the expression profile of each gene
 ExpressionProfile = ParseGTEXExpression('GTEX_Median_Normalized_FPKM.txt')
 # remove genes without any expression
@@ -106,10 +89,8 @@ ExpressionProfile = RemoveGenesLackingExpression(ExpressionProfile)
 # transform absulte expression in relative expression
 ExpressionProfile = TransformRelativeExpression(ExpressionProfile)
 
-
 # make a list of all gene sets
-AllGeneSets = [NonOverlappingGenes, InternalSameGenes, InternalOppositeGenes, ExternalSameGenes,
-               ExternalOppositeGenes, PiggyBackGenes, ConvergentGenes, DivergentGenes]
+AllGeneSets = [NonOverlappingGenes, NestedGenes, PiggyBackGenes, ConvergentGenes, DivergentGenes]
 
 # make a list of tissues
 infile = open('GTEX_Median_Normalized_FPKM.txt')
@@ -129,8 +110,7 @@ for gene in Expressed:
 
 
 # make a list of gene category names parallel to the list of gene sets
-GeneCats = ['NoOv', 'CisInt', 'TransInt', 'CisExt', 'TransExt', 'Pbk', 'Conv', 'Div']
-
+GeneCats = ['NoOv', 'Nst', 'Pbk', 'Conv', 'Div']
 
 # create a dict to count the number of genes in each category with highest expression in each tissue
 HighestExpression = {}
@@ -156,11 +136,6 @@ for i in range(len(GeneCats)):
     for j in range(len(HighestExpression[GeneCats[i]])):
         HighestExpression[GeneCats[i]][j] = round(HighestExpression[GeneCats[i]][j] / len(AllGeneSets[i]), 4)
 
-
-
-for i in range(len(Tissues)):
-    print(Tissues[i], HighestExpression['NoOv'][i], HighestExpression['CisInt'][i], HighestExpression['TransInt'][i], HighestExpression['CisExt'][i], HighestExpression['TransExt'][i], HighestExpression['Pbk'][i], HighestExpression['Conv'][i], HighestExpression['Div'][i], sep = '\t')
-
 # create a dictionary with tissue as key and a list of gene proportions for each gene category as value
 Proportions = {}
 for i in range(len(Tissues)):
@@ -170,7 +145,7 @@ for i in range(len(Tissues)):
 
 
 # create a function to format the subplots
-def CreateAx(Columns, Rows, Position, figure, Proportions, Title, YMax, YLabel):
+def CreateAx(Columns, Rows, Position, figure, Data, XLabel, YMax, YLabel):
     '''
     (int, int, int, list, figure_object, str, int, list, list)
     Take the number of a column, and rows in the figure object and the position of
@@ -178,72 +153,70 @@ def CreateAx(Columns, Rows, Position, figure, Proportions, Title, YMax, YLabel):
     a list with species names and list of X axis tick positions and return an
     ax instance in the figure
     '''    
-    
-    # create subplot in figure
     # add a plot to figure (N row, N column, plot N)
     ax = figure.add_subplot(Rows, Columns, Position)
     # set colors
-    colorscheme = ['lightgrey'] * 8
+    colorscheme = ['#fb9a99', '#a6cee3','#1f78b4','#b2df8a','#33a02c']
     # plot nucleotide divergence
-    ax.bar([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7], Proportions, 0.1, color = colorscheme,
-           edgecolor = 'black', linewidth = 1,
+    ax.bar([0, 0.2, 0.4, 0.6, 0.8], Data, 0.2, color = colorscheme,
+           edgecolor = 'black', linewidth = 0.5,
            error_kw=dict(elinewidth=1, ecolor='black', markeredgewidth = 1))
-
     # set font for all text in figure
     FigFont = {'fontname':'Arial'}   
     # write y axis label
     if YLabel == True:
         ax.set_ylabel('Proportions', color = 'black',  size = 7, ha = 'center', **FigFont)
-    
-    #ax.set_xlabel(XLabel, color = 'black',  size = 8, ha = 'center', **FigFont)
-        
-    plt.title(Title, size = 7, color = 'black', ha = 'center', **FigFont )     
-    
-    
-#    # write x ticks
-#    plt.xticks([0.05, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45], ['br', 'cb', 'ht', 'kd', 'lv', 'ts'], ha = 'center', fontsize = 8, **FigFont)
+    ax.set_xlabel(XLabel, color = 'black',  size = 7, ha = 'center', **FigFont)
     
     # add a range for the Y axis
     plt.ylim([0, YMax])
-       
+    
     # do not show lines around figure  
     ax.spines["top"].set_visible(False)    
     ax.spines["bottom"].set_visible(False)    
-    ax.spines["right"].set_visible(False)    
-    ax.spines["left"].set_visible(True)  
-        
-    # do not show ticks
-    plt.tick_params(
-        axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-        which='both',      # both major and minor ticks are affected
-        bottom='on',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        right = 'off',
-        left = 'on',          
-        labelbottom='on', # labels along the bottom edge are on
-        colors = 'black',
-        labelsize = 7,
-        direction = 'out') # ticks are outside the frame when bottom = 'on'  
-         
+    ax.spines["right"].set_visible(False)
+    if YLabel == True:
+        ax.spines["left"].set_visible(True)  
+    elif YLabel == False:
+        ax.spines['left'].set_visible(False)
+    
+    # edit tick parameters    
+    if YLabel == True:
+        plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                        right = 'off', left = 'on', labelbottom='off', 
+                        colors = 'black', labelsize = 7, direction = 'out')  
+    elif YLabel == False:
+        plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                        right = 'off', left = 'off', labelleft='off', labelbottom='off', 
+                        colors = 'black', labelsize = 7, direction = 'out')  
     # Set the tick labels font name
     for label in ax.get_yticklabels():
         label.set_fontname('Arial')   
-     
     return ax      
 
 
 # create figure
-fig = plt.figure(1, figsize = (8, 5))
+fig = plt.figure(1, figsize = (5, 3))
 
 
 j = 1
 for i in range(len(Tissues)):
-    if i == 0 or i == 16:
+    tissue = Tissues[i]
+    if j == 1 or j == 11 or j == 21:
         YLabel = True
     else:
         YLabel = False
-    ax = CreateAx(15, 2, j, fig, Proportions[Tissues[i]], Tissues[i].lower().replace('_', ' '), 0.3, YLabel)
+    ax = CreateAx(10, 3, j, fig, Proportions[tissue], tissue.lower().replace('_', '\n'), 0.31, YLabel)
     j += 1
+
+
+# adjust padding between subplots
+# pad controls the padding around the figure border
+# hpad and wpad control the padding between subplots
+plt.tight_layout(pad=0.3, w_pad=0.2, h_pad=0.5)
+
+
+
 
 
 #black_line = mlines.Line2D([], [], color='black', marker='', linewidth = 1.2, label = Label1)
@@ -257,8 +230,6 @@ for i in range(len(Tissues)):
 #U = mpatches.Patch(facecolor = '#b2df8a', edgecolor = 'black', linewidth = 1, label= 'Control')
 #ax1.legend(handles = [H, N, U], loc = (0, 1), fontsize = 8, frameon = False, ncol = 3)
 
-# make sure subplots do not overlap
-plt.tight_layout()
 
 fig.savefig('truc.pdf', bbox_inches = 'tight')
 #fig.savefig(outputfile + '.eps', bbox_inches = 'tight')
