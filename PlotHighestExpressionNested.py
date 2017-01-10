b781c6d825a233f0b731934e7f314f2d2ab57038
Richard <richard.jovelin@oicr.on.ca>
2017-01-10 18:06:53 -0500

    plot distribution of tissues with highest expression for nested genes

--- PlotHighestExpressionNested.py
+++ PlotHighestExpressionNested.py
@@ -12,10 +12,10 @@ Created on Wed Nov 23 15:14:49 2016
 @author: RJovelin
 """
 
-# use this script to plot the proportion of overlapping and non-overlapping genes
+# use this script to plot the proportion of external and internal nested genes
 # with highest expression in each tissue
 
-# usage python3 PlotHighestExpression.py 
+# usage python3 PlotHighestExpressionNested.py 
 
 
 # import modules
@@ -45,18 +45,6 @@ json_data.close()
 json_data = open('HumanNestedGenes.json')
 Nested = json.load(json_data)
 json_data.close()
-# load dictionary of pibbyback gene pairs
-json_data = open('HumanPiggyBackGenes.json')
-Piggyback = json.load(json_data)
-json_data.close()
-# load dictionary of convergent gene pairs
-json_data = open('HumanConvergentGenes.json')
-Convergent = json.load(json_data)
-json_data.close()
-# load dictionary of divergent gene pairs
-json_data = open('HumanDivergentGenes.json')
-Divergent = json.load(json_data)
-json_data.close()
 
 # get GFF file
 GFF = 'Homo_sapiens.GRCh38.86.gff3'
@@ -70,22 +58,12 @@ GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneT
 # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
 GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
 
-
 # create lists of gene pairs
 OverlappingPairs = GetHostNestedPairs(Overlapping)
 NestedPairs = GetHostNestedPairs(Nested)
-PiggybackPairs = GetHostNestedPairs(Piggyback)
-ConvergentPairs = GetHostNestedPairs(Convergent)
-DivergentPairs = GetHostNestedPairs(Divergent)
 
 # generate gene sets
 NestedGenes  = MakeFullPartialOverlapGeneSet(Nested)
-OverlappingGenes = MakeFullPartialOverlapGeneSet(Overlapping)
-ConvergentGenes = MakeFullPartialOverlapGeneSet(Convergent)
-DivergentGenes = MakeFullPartialOverlapGeneSet(Divergent)
-PiggyBackGenes = MakeFullPartialOverlapGeneSet(Piggyback)
-
-# make a set of non-overlapping genes
 NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlapping, GeneCoord)
 
 # create lists of nested gene pairs with same and opposite directions
@@ -105,7 +83,6 @@ for pair in opposite:
     ExternalOppositeGenes.add(pair[0])
     InternalOppositeGenes.add(pair[1])
 
-
 # parse the GTEX expression summary file to obtain the expression profile of each gene
 ExpressionProfile = ParseGTEXExpression('GTEX_Median_Normalized_FPKM.txt')
 # remove genes without any expression
@@ -113,10 +90,8 @@ ExpressionProfile = RemoveGenesLackingExpression(ExpressionProfile)
 # transform absulte expression in relative expression
 ExpressionProfile = TransformRelativeExpression(ExpressionProfile)
 
-
 # make a list of all gene sets
-AllGeneSets = [NonOverlappingGenes, InternalSameGenes, InternalOppositeGenes, ExternalSameGenes,
-               ExternalOppositeGenes, PiggyBackGenes, ConvergentGenes, DivergentGenes]
+AllGeneSets = [NonOverlappingGenes, InternalSameGenes, InternalOppositeGenes, ExternalSameGenes, ExternalOppositeGenes]
 
 # make a list of tissues
 infile = open('GTEX_Median_Normalized_FPKM.txt')
@@ -127,7 +102,6 @@ for i in range(len(Tissues)):
     if ' ' in Tissues[i]:
         Tissues[i] = Tissues[i].replace(' ', '_')
 
-
 # make a list of genes with expression
 Expressed = list(ExpressionProfile.keys())
 # check that all genes have the same number of tissues
@@ -136,7 +110,7 @@ for gene in Expressed:
 
 
 # make a list of gene category names parallel to the list of gene sets
-GeneCats = ['NoOv', 'CisInt', 'TransInt', 'CisExt', 'TransExt', 'Pbk', 'Conv', 'Div']
+GeneCats = ['NoOv', 'CisInt', 'TransInt', 'CisExt', 'TransExt']
 
 
 # create a dict to count the number of genes in each category with highest expression in each tissue
@@ -165,9 +139,6 @@ for i in range(len(GeneCats)):
 
 
 
-for i in range(len(Tissues)):
-    print(Tissues[i], HighestExpression['NoOv'][i], HighestExpression['CisInt'][i], HighestExpression['TransInt'][i], HighestExpression['CisExt'][i], HighestExpression['TransExt'][i], HighestExpression['Pbk'][i], HighestExpression['Conv'][i], HighestExpression['Div'][i], sep = '\t')
-
 # create a dictionary with tissue as key and a list of gene proportions for each gene category as value
 Proportions = {}
 for i in range(len(Tissues)):
@@ -177,7 +148,7 @@ for i in range(len(Tissues)):
 
 
 # create a function to format the subplots
-def CreateAx(Columns, Rows, Position, figure, Proportions, Title, YMax, YLabel):
+def CreateAx(Columns, Rows, Position, figure, Data, XLabel, YMax, YLabel):
     '''
     (int, int, int, list, figure_object, str, int, list, list)
     Take the number of a column, and rows in the figure object and the position of
@@ -185,87 +156,76 @@ def CreateAx(Columns, Rows, Position, figure, Proportions, Title, YMax, YLabel):
     a list with species names and list of X axis tick positions and return an
     ax instance in the figure
     '''    
-    
-    # create subplot in figure
     # add a plot to figure (N row, N column, plot N)
     ax = figure.add_subplot(Rows, Columns, Position)
     # set colors
-    colorscheme = ['lightgrey'] * 8
+    colorscheme = ['#fb9a99', '#9ecae1','#3182bd', '#a1d99b','#31a354'] 
+    
     # plot nucleotide divergence
-    ax.bar([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7], Proportions, 0.1, color = colorscheme,
-           edgecolor = 'black', linewidth = 1,
+    ax.bar([0, 0.2, 0.4, 0.6, 0.8], Data, 0.2, color = colorscheme,
+           edgecolor = 'black', linewidth = 0.5,
            error_kw=dict(elinewidth=1, ecolor='black', markeredgewidth = 1))
-
     # set font for all text in figure
     FigFont = {'fontname':'Arial'}   
     # write y axis label
     if YLabel == True:
-        ax.set_ylabel('Proportions', color = 'black',  size = 7, ha = 'center', **FigFont)
-    
-    #ax.set_xlabel(XLabel, color = 'black',  size = 8, ha = 'center', **FigFont)
-        
-    plt.title(Title, size = 7, color = 'black', ha = 'center', **FigFont )     
-    
-    
-#    # write x ticks
-#    plt.xticks([0.05, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45], ['br', 'cb', 'ht', 'kd', 'lv', 'ts'], ha = 'center', fontsize = 8, **FigFont)
+        ax.set_ylabel('Proportion', color = 'black',  size = 7, ha = 'center', **FigFont)
+    ax.set_xlabel(XLabel, color = 'black',  size = 7, ha = 'center', **FigFont)
     
     # add a range for the Y axis
     plt.ylim([0, YMax])
-       
+    
     # do not show lines around figure  
     ax.spines["top"].set_visible(False)    
     ax.spines["bottom"].set_visible(False)    
-    ax.spines["right"].set_visible(False)    
-    ax.spines["left"].set_visible(True)  
-        
-    # do not show ticks
-    plt.tick_params(
-        axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-        which='both',      # both major and minor ticks are affected
-        bottom='on',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        right = 'off',
-        left = 'on',          
-        labelbottom='on', # labels along the bottom edge are on
-        colors = 'black',
-        labelsize = 7,
-        direction = 'out') # ticks are outside the frame when bottom = 'on'  
-         
+    ax.spines["right"].set_visible(False)
+    if YLabel == True:
+        ax.spines["left"].set_visible(True)  
+    elif YLabel == False:
+        ax.spines['left'].set_visible(False)
+    
+    # edit tick parameters    
+    if YLabel == True:
+        plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                        right = 'off', left = 'on', labelbottom='off', 
+                        colors = 'black', labelsize = 7, direction = 'out')  
+    elif YLabel == False:
+        plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                        right = 'off', left = 'off', labelleft='off', labelbottom='off', 
+                        colors = 'black', labelsize = 7, direction = 'out')  
     # Set the tick labels font name
     for label in ax.get_yticklabels():
         label.set_fontname('Arial')   
-     
     return ax      
 
 
 # create figure
-fig = plt.figure(1, figsize = (8, 5))
+fig = plt.figure(1, figsize = (5, 3))
 
-
+# plot data
 j = 1
 for i in range(len(Tissues)):
-    if i == 0 or i == 16:
+    tissue = Tissues[i]
+    if j == 1 or j == 11 or j == 21:
         YLabel = True
     else:
         YLabel = False
-    ax = CreateAx(15, 2, j, fig, Proportions[Tissues[i]], Tissues[i].lower().replace('_', ' '), 0.3, YLabel)
+    ax = CreateAx(10, 3, j, fig, Proportions[tissue], tissue.lower().replace('_', '\n'), 0.31, YLabel)
     j += 1
 
-
-#black_line = mlines.Line2D([], [], color='black', marker='', linewidth = 1.2, label = Label1)
-#grey_line = mlines.Line2D([], [], color='red', marker='', linewidth = 1.2, label = 'Overlapping')
-#plt.legend(handles=[black_line, grey_line], bbox_to_anchor=(-12, 0.8), loc = 3, ncol = 2, fontsize = 10, frameon = False, borderaxespad = 0.)
-#
-#
-## add legend relative to ax1 using ax1 coordinates
-#H = mpatches.Patch(facecolor = '#a6cee3', edgecolor = 'black', linewidth = 1, label= 'Hosts')
-#N = mpatches.Patch(facecolor = '#1f78b4', edgecolor = 'black', linewidth = 1, label= 'Nested')
-#U = mpatches.Patch(facecolor = '#b2df8a', edgecolor = 'black', linewidth = 1, label= 'Control')
-#ax1.legend(handles = [H, N, U], loc = (0, 1), fontsize = 8, frameon = False, ncol = 3)
-
-# make sure subplots do not overlap
-plt.tight_layout()
+    # create legend
+    if j == 10:
+        No = mpatches.Patch(facecolor = '#fb9a99', edgecolor = 'black', linewidth = 0.5, label= 'NoOv')
+        cisint = mpatches.Patch(facecolor = '#9ecae1', edgecolor = 'black', linewidth = 0.5, label= 'CisInt')
+        transint = mpatches.Patch(facecolor = '#3182bd', edgecolor = 'black', linewidth = 0.5, label= 'TransInt')
+        cisopp = mpatches.Patch(facecolor = '#a1d99b', edgecolor = 'black', linewidth = 0.5, label= 'CisExt')
+        transopp = mpatches.Patch(facecolor = '#31a354', edgecolor = 'black', linewidth = 0.5, label= 'TransExt')
+        ax.legend(handles = [No, cisint, transint, cisopp, transopp], bbox_to_anchor=(-10, 0.6), loc = 3, fontsize = 6, frameon = False, ncol = 5)
+             
+# adjust padding between subplots
+# pad controls the padding around the figure border
+# hpad and wpad control the padding between subplots
+plt.tight_layout(pad=0.2, w_pad=0, h_pad=0.5)
 
 fig.savefig('truc.pdf', bbox_inches = 'tight')
-#fig.savefig(outputfile + '.eps', bbox_inches = 'tight')
+
