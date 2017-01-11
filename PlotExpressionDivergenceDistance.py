b3af4f46bdae62d19ef6dade1c94aa61d456f7aa
Richard <richard.jovelin@oicr.on.ca>
2017-01-11 17:02:56 -0500

    fixed typo

--- PlotExpressionDivergenceDistance.py
+++ PlotExpressionDivergenceDistance.py
@@ -117,46 +117,29 @@ fig = plt.figure(1, figsize = (3, 2))
 # add a plot to figure (N row, N column, plot N)
 ax = fig.add_subplot(1, 1, 1)
 # set colors
-#colorscheme = ['#fb9a99', '#a6cee3','#1f78b4','#b2df8a','#33a02c', 'lightgrey', 'lightgrey', 'lightgrey', 'lightgrey']
 colorscheme = ['grey','grey','grey','grey', 'lightgrey', 'lightgrey', 'lightgrey', 'lightgrey']
-
-
-#No = mpatches.Patch(facecolor = '#fb9a99', edgecolor = 'black', linewidth = 0.5, label= 'NoOv')
-#Ns = mpatches.Patch(facecolor = '#a6cee3', edgecolor = 'black', linewidth = 0.5, label= 'Nst')
-#Pk = mpatches.Patch(facecolor = '#1f78b4', edgecolor = 'black', linewidth = 0.5, label= 'Pbk')
-#Co = mpatches.Patch(facecolor = '#b2df8a', edgecolor = 'black', linewidth = 0.5, label= 'Conv')
-#Dv = mpatches.Patch(facecolor = '#33a02c', edgecolor = 'black', linewidth = 0.5, label= 'Div')
-#ax.legend(handles = [No, Ns, Pk, Co, Dv], bbox_to_anchor=(-10, 0.6), loc = 3, fontsize = 6, frameon = False, ncol = 5)
-
-
 # plot nucleotide divergence
-ax.bar([0.5, 0.35, 0.65, 0.95, 1.25, 1.55, 1.85, 2.15], MeanExpDiv, 0.2, yerr = SEMExpDiv, color = colorscheme,
+ax.bar([0.05, 0.35, 0.65, 0.95, 1.25, 1.55, 1.85, 2.15], MeanExpDiv, 0.2, yerr = SEMExpDiv, color = colorscheme,
        edgecolor = 'black', linewidth = 0.5,
        error_kw=dict(elinewidth=0.5, ecolor='black', markeredgewidth = 0.5))
-
 # set font for all text in figure
 FigFont = {'fontname':'Arial'}   
 # write y axis label
 ax.set_ylabel('Expression divergence', color = 'black',  size = 7, ha = 'center', **FigFont)
-
 # add ticks and lebels
 plt.xticks([0.15, 0.45, 0.75, 15, 1.35, 1.65, 1.95, 2.25], GeneCats, size = 7, color = 'black', ha = 'center', **FigFont)
-
 # add a range for the Y and X axes
 plt.ylim([0, 0.6])
 plt.xlim([0, 2.5])
-
 # do not show lines around figure  
 ax.spines["top"].set_visible(False)    
 ax.spines["bottom"].set_visible(True)    
 ax.spines["right"].set_visible(False)
 ax.spines["left"].set_visible(True)  
-
 # edit tick parameters    
 plt.tick_params(axis='both', which='both', bottom='on', top='off',
                 right = 'off', left = 'on', labelbottom='on',
                 colors = 'black', labelsize = 7, direction = 'out')  
-
 # Set the tick labels font name
 for label in ax.get_yticklabels():
     label.set_fontname('Arial')   
