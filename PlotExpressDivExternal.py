ccdd6e6496c19157f9db12cdd0eb7827731e073e
Richard <richard.jovelin@oicr.on.ca>
2017-01-25 17:11:58 -0500

    added print statement

--- PlotExpressDivExternal.py
+++ PlotExpressDivExternal.py
@@ -249,8 +249,10 @@ POrthosExprx = []
 for i in range(0, len(OrthoExprxDiverg), 2):
     P = PermutationResampling(OrthoExprxDiverg[i], OrthoExprxDiverg[i+1], 10000, np.mean)
     POrthosExprx.append(P)
-for i in POrthosExprx:
-    print(i)
+
+
+print(MeanOrthoExpDiv)
+print(POrthosExprx)
 
     
   
@@ -258,183 +260,73 @@ for i in POrthosExprx:
 
 
 
+
+## convert p-values to star significance level
+#Significance = []
+#for pvalue in PValues:
+#    if pvalue >= 0.05:
+#        Significance.append('')
+#    elif pvalue < 0.05 and pvalue >= 0.01:
+#        Significance.append('*')
+#    elif pvalue < 0.01 and pvalue >= 0.001:
+#        Significance.append('**')
+#    elif pvalue < 0.001:
+#        Significance.append('***')
+
+
+
 ###############################
 
-## create figure
-#fig = plt.figure(1, figsize = (3, 2))
-#
-## add a plot to figure (N row, N column, plot N)
-#ax = fig.add_subplot(1, 1, 1)
-## set colors
-#colorscheme = ['grey','grey','grey','grey', 'lightgrey', 'lightgrey', 'lightgrey', 'lightgrey']
-## plot nucleotide divergence
-#ax.bar([0.05, 0.35, 0.65, 0.95, 1.25, 1.55, 1.85, 2.15], MeanExpDiv, 0.2, yerr = SEMExpDiv, color = colorscheme,
-#       edgecolor = 'black', linewidth = 0.5,
-#       error_kw=dict(elinewidth=0.5, ecolor='black', markeredgewidth = 0.5))
-## set font for all text in figure
-#FigFont = {'fontname':'Arial'}   
-## write y axis label
-#ax.set_ylabel('Expression divergence', color = 'black',  size = 7, ha = 'center', **FigFont)
-## add ticks and lebels
-#plt.xticks([0.15, 0.45, 0.75, 1.05, 1.35, 1.65, 1.95, 2.25], GeneCats, size = 7, color = 'black', ha = 'center', **FigFont)
-## add a range for the Y and X axes
-#plt.ylim([0, 0.61])
-#plt.xlim([0, 2.45])
-## do not show lines around figure  
-#ax.spines["top"].set_visible(False)    
-#ax.spines["bottom"].set_visible(True)    
-#ax.spines["right"].set_visible(False)
-#ax.spines["left"].set_visible(True)  
-## edit tick parameters    
-#plt.tick_params(axis='both', which='both', bottom='on', top='off',
-#                right = 'off', left = 'on', labelbottom='on',
-#                colors = 'black', labelsize = 7, direction = 'out')  
-## Set the tick labels font name
-#for label in ax.get_yticklabels():
-#    label.set_fontname('Arial')   
-#      
-#
-#
-#
-## annotate figure to add significance
-## significant comparisons were already determined, add letters to show significance
-#Diff = ['A', 'B', 'C', 'B', 'D', 'E', 'F', 'G']
-#ypos = [0.55] * 8
-#xpos = [0.15, 0.45, 0.75, 1.05, 1.35, 1.65, 1.95, 2.25]
-#for i in range(len(Diff)):
-#    ax.text(xpos[i], ypos[i], Diff[i], ha='center', va='center', color = 'black', fontname = 'Arial', size = 7)
-#    
-## save figure
-#fig.savefig('truc.pdf', bbox_inches = 'tight')
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, figure, Data, XLabel, YLabel, DataType, YMax):
+    '''
+    Returns a ax instance in figure
+    '''    
 
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    # check type of graphic    
+    if DataType == 'divergence':
+        # set colors
+        colorscheme = ['black','lightgrey','lightgrey','lightgrey', 'lightgrey', 'lightgrey', 'lightgrey']
+        # plot nucleotide divergence
+        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[0], 0.2, yerr = Data[1], color = colorscheme,
+               edgecolor = 'black', linewidth = 0.7, error_kw=dict(elinewidth=0.7, ecolor='black', markeredgewidth = 0.7))
+    elif DataType == 'proportion':
+        ## Create a horizontal bar plot for proportions of genes with homologs
+        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[0], width = 0.2, label = 'homolog', color= 'black', linewidth = 0.7)
+        # Create a horizontal bar plot for proportions of same strand pairs
+        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[1], width = 0.2, bottom = Data[0], label = 'no homolog', color= 'lightgrey', linewidth = 0.7)
 
-
-###########################
-
-
+    # set font for all text in figure
+    FigFont = {'fontname':'Arial'}   
+    # write y axis label
+    ax.set_ylabel(YLabel, color = 'black',  size = 7, ha = 'center', **FigFont)
+    # add ticks and lebels
+    plt.xticks([0.1, 0.4, 0.7, 1, 1.3, 1.6, 1.9], XLabel, rotation = 30, size = 7, color = 'black', ha = 'right', **FigFont)
+    # add a range for the Y and X axes
+    plt.ylim([0, YMax])    
     
+    # do not show lines around figure  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(True)    
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(True)  
+    # edit tick parameters    
+    plt.tick_params(axis='both', which='both', bottom='on', top='off',
+                    right = 'off', left = 'on', labelbottom='on',
+                    colors = 'black', labelsize = 7, direction = 'out')  
+    # Set the tick labels font name
+    for label in ax.get_yticklabels():
+        label.set_fontname('Arial')   
+      
+    # add margins
+    plt.margins(0.1)
     
+    return ax
 
-## perform statistical tests between gene categories using Kolmogorov-Smirnof test
-## create list to store the p-values
-#PValOmega = []
-#for i in range(1, len(Omega)):
-#    # compare each gene category to non-overlapping genes
-#    val, P = stats.ks_2samp(Omega[0], Omega[i])
-#    PValOmega.append(P)
-## replace P values with significance
-#for i in range(len(PValOmega)):
-#    if PValOmega[i] >= 0.05:
-#        PValOmega[i] = ''
-#    elif PValOmega[i] < 0.05 and PValOmega[i] >= 0.01:
-#        PValOmega[i] = '*'
-#    elif PValOmega[i] < 0.01 and PValOmega[i] >= 0.001:
-#        PValOmega[i] = '**'
-#    elif PValOmega[i] < 0.001:
-#        PValOmega[i] = '***'
-#
-## create a set of human genes that have homologs
-## include 1:1 and 1 to many or many to many orthologs
-#Homologs = set()
-#infile = open('HumanChimpOrthologs.txt')
-#infile.readline()
-#for line in infile:
-#    if 'ortholog' in line:
-#        line = line.rstrip().split('\t')
-#        assert 'ENS' in line[0], 'gene id is not valid'
-#        assert 'ortholog' in line[4], 'ortholog should be in homology type'
-#        Homologs.add(line[0])
-#infile.close()            
-#
-## count genes with and without homologs
-#GeneCounts = []
-## loop over gene sets
-#for i in range(len(AllGenes)):
-#    # initialize counters
-#    homo, nohomo = 0, 0
-#    # loop over genes in given set
-#    for gene in AllGenes[i]:
-#        if gene in Homologs:
-#            homo += 1
-#        else:
-#            nohomo += 1
-#    GeneCounts.append([homo, nohomo])    
-#
-## compare the proportions of gene with and without homologs
-## create a list to store the P-values
-#PProp = []
-#for i in range(1, len(GeneCounts)):
-#    p = stats.fisher_exact([GeneCounts[0], GeneCounts[i]])[1]
-#    PProp.append(p)
-## replace P values by significance
-#for i in range(len(PProp)):
-#    if PProp[i] >= 0.05:
-#        PProp[i] = ''
-#    elif PProp[i] < 0.05 and PProp[i] >= 0.01:
-#        PProp[i] = '*'
-#    elif PProp[i] < 0.01 and PProp[i] >= 0.001:
-#        PProp[i] = '**'
-#    elif PProp[i] < 0.001:
-#        PProp[i] = '***'
-#
-## get the proportions of genes with and without homologs
-#WithHomolog, NoHomolog = [], []
-#for i in range(len(GeneCounts)):
-#    WithHomolog.append(GeneCounts[i][0] / sum(GeneCounts[i]))
-#    NoHomolog.append(GeneCounts[i][1] / sum(GeneCounts[i]))
-#    assert sum(GeneCounts[i]) == len(AllGenes[i])
-#
-#
-## create a function to format the subplots
-#def CreateAx(Columns, Rows, Position, figure, Data, XLabel, YLabel, DataType, YMax):
-#    '''
-#    Returns a ax instance in figure
-#    '''    
-#
-#    # add a plot to figure (N row, N column, plot N)
-#    ax = figure.add_subplot(Rows, Columns, Position)
-#    # check type of graphic    
-#    if DataType == 'divergence':
-#        # set colors
-#        colorscheme = ['black','lightgrey','lightgrey','lightgrey', 'lightgrey', 'lightgrey', 'lightgrey']
-#        # plot nucleotide divergence
-#        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[0], 0.2, yerr = Data[1], color = colorscheme,
-#               edgecolor = 'black', linewidth = 0.7, error_kw=dict(elinewidth=0.7, ecolor='black', markeredgewidth = 0.7))
-#    elif DataType == 'proportion':
-#        ## Create a horizontal bar plot for proportions of genes with homologs
-#        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[0], width = 0.2, label = 'homolog', color= 'black', linewidth = 0.7)
-#        # Create a horizontal bar plot for proportions of same strand pairs
-#        ax.bar([0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8], Data[1], width = 0.2, bottom = Data[0], label = 'no homolog', color= 'lightgrey', linewidth = 0.7)
-#
-#    # set font for all text in figure
-#    FigFont = {'fontname':'Arial'}   
-#    # write y axis label
-#    ax.set_ylabel(YLabel, color = 'black',  size = 7, ha = 'center', **FigFont)
-#    # add ticks and lebels
-#    plt.xticks([0.1, 0.4, 0.7, 1, 1.3, 1.6, 1.9], XLabel, rotation = 30, size = 7, color = 'black', ha = 'right', **FigFont)
-#    # add a range for the Y and X axes
-#    plt.ylim([0, YMax])    
-#    
-#    # do not show lines around figure  
-#    ax.spines["top"].set_visible(False)    
-#    ax.spines["bottom"].set_visible(True)    
-#    ax.spines["right"].set_visible(False)
-#    ax.spines["left"].set_visible(True)  
-#    # edit tick parameters    
-#    plt.tick_params(axis='both', which='both', bottom='on', top='off',
-#                    right = 'off', left = 'on', labelbottom='on',
-#                    colors = 'black', labelsize = 7, direction = 'out')  
-#    # Set the tick labels font name
-#    for label in ax.get_yticklabels():
-#        label.set_fontname('Arial')   
-#      
-#    # add margins
-#    plt.margins(0.1)
-#    
-#    return ax
-#
-#
-#
+
+
 ## make a figure with mean dN/dS and with proportion of gene with homologs
 #
 ## create figure
@@ -464,14 +356,4 @@ for i in POrthosExprx:
 
 
 
-## convert p-values to star significance level
-#Significance = []
-#for pvalue in PValues:
-#    if pvalue >= 0.05:
-#        Significance.append('')
-#    elif pvalue < 0.05 and pvalue >= 0.01:
-#        Significance.append('*')
-#    elif pvalue < 0.01 and pvalue >= 0.001:
-#        Significance.append('**')
-#    elif pvalue < 0.001:
-#        Significance.append('***')
+
