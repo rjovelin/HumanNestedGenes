d2f979891eb22939594b4a958606c6d4c10929d4
Richard <richard.jovelin@oicr.on.ca>
2017-02-01 16:41:38 -0500

    plot proportions of genes with highest expression in each tissu

--- PlotHighestExprexIntron.py
+++ PlotHighestExprexIntron.py
@@ -50,31 +50,43 @@ MapGeneTranscript = GeneToTranscripts(GFF)
 GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
 # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
 GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
-
-# create lists of gene pairs
-OverlappingPairs = GetHostNestedPairs(Overlapping)
-NestedPairs = GetHostNestedPairs(Nested)
+# Map Transcript names to gene names {transcript: gene}
+MapTranscriptGene = TranscriptToGene(GFF)
+# get the coordinates of all exons    
+ExonCoord = GeneExonCoord(GFF)
+ExonCoord = CleanGeneFeatureCoord(ExonCoord, MapTranscriptGene)
+# get the intron coordinates of all transcripts {transcript: [[intron_start, intron_end]]}
+IntronCoord = GeneIntronCoord(ExonCoord)
+IntronCoord = CleanGeneFeatureCoord(IntronCoord, MapTranscriptGene)
+# get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
+TranscriptCoordinates = TranscriptsCoord(GFF)
+# map genes to their longest transcript {gene: longest_transcript}
+GeneLongestTranscript = LongestTranscript(TranscriptCoordinates, MapGeneTranscript)
+# match longest transcript of the nested genes to transcript of the host gene (longest transcript in priority)
+Matches = MatchHostTranscriptWithNestedTranscript(Nested, MapGeneTranscript, GeneLongestTranscript, TranscriptCoordinates, IntronCoord)
+# make a set of un-nested genes
+UnNestedGenes = MakeNonOverlappingGeneSet(Nested, GeneCoord)
+# list all host, nested transcript pairs [[host, nested]]
+HostNestedPairs = GetHostNestedPairs(Matches)
 
 # generate gene sets
-NestedGenes  = MakeFullPartialOverlapGeneSet(Nested)
 NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlapping, GeneCoord)
 
-# create lists of nested gene pairs with same and opposite directions
-same, opposite = [], []
-for pair in NestedPairs:
-    orientation = GenePairOrientation(pair, GeneCoord)
-    if len(set(orientation)) == 2:
-        opposite.append(pair)
-    elif len(set(orientation)) == 1:
-        same.append(pair)
-# create sets of internal and external nested genes depending on orientation 
-InternalSameGenes, InternalOppositeGenes, ExternalSameGenes, ExternalOppositeGenes = set(), set(), set(), set()
-for pair in same:
-    ExternalSameGenes.add(pair[0])
-    InternalSameGenes.add(pair[1])
-for pair in opposite:
-    ExternalOppositeGenes.add(pair[0])
-    InternalOppositeGenes.add(pair[1])
+# make sets of external genes with intronless internal genes or intron-containing genes
+# make sets of internal intronless genes and internal genes with introns
+ExternalIntronless, ExternalWithIntron = set(), set()
+InternalIntronless, InternalWithIntron = set(), set()
+
+for i in range(len(HostNestedPairs)):
+    # get the external and internal transcripts of the pair
+    external, internal = HostNestedPairs[i][0], HostNestedPairs[i][1]
+    # check if internal transcript has intron
+    if internal in IntronCoord:
+        ExternalWithIntron.add(MapTranscriptGene[external])
+        InternalWithIntron.add(MapTranscriptGene[internal])
+    else:
+        ExternalIntronless.add(MapTranscriptGene[external])
+        InternalIntronless.add(MapTranscriptGene[internal])
 
 # parse the GTEX expression summary file to obtain the expression profile of each gene
 ExpressionProfile = ParseGTEXExpression('GTEX_Median_Normalized_FPKM.txt')
@@ -84,7 +96,7 @@ ExpressionProfile = RemoveGenesLackingExpression(ExpressionProfile)
 ExpressionProfile = TransformRelativeExpression(ExpressionProfile)
 
 # make a list of all gene sets
-AllGeneSets = [NonOverlappingGenes, InternalSameGenes, InternalOppositeGenes, ExternalSameGenes, ExternalOppositeGenes]
+AllGeneSets = [NonOverlappingGenes, InternalIntronless, InternalWithIntron, ExternalIntronless, ExternalWithIntron]
 
 # make a list of tissues
 infile = open('GTEX_Median_Normalized_FPKM.txt')
@@ -101,10 +113,8 @@ Expressed = list(ExpressionProfile.keys())
 for gene in Expressed:
     assert len(ExpressionProfile[gene]) == len(Tissues)
 
-
 # make a list of gene category names parallel to the list of gene sets
-GeneCats = ['NoOv', 'CisInt', 'TransInt', 'CisExt', 'TransExt']
-
+GeneCats = ['NoOv', 'IntN', 'IntW', 'ExtN', 'ExtW']
 
 # create a dict to count the number of genes in each category with highest expression in each tissue
 HighestExpression = {}
@@ -129,8 +139,6 @@ for i in range(len(AllGeneSets)):
 for i in range(len(GeneCats)):
     for j in range(len(HighestExpression[GeneCats[i]])):
         HighestExpression[GeneCats[i]][j] = round(HighestExpression[GeneCats[i]][j] / len(AllGeneSets[i]), 4)
-
-
 
 # create a dictionary with tissue as key and a list of gene proportions for each gene category as value
 Proportions = {}
@@ -209,252 +217,16 @@ for i in range(len(Tissues)):
     # create legend
     if j == 10:
         No = mpatches.Patch(facecolor = '#fb9a99', edgecolor = 'black', linewidth = 0.5, label= 'NoOv')
-        cisint = mpatches.Patch(facecolor = '#9ecae1', edgecolor = 'black', linewidth = 0.5, label= 'CisInt')
-        transint = mpatches.Patch(facecolor = '#3182bd', edgecolor = 'black', linewidth = 0.5, label= 'TransInt')
-        cisopp = mpatches.Patch(facecolor = '#a1d99b', edgecolor = 'black', linewidth = 0.5, label= 'CisExt')
-        transopp = mpatches.Patch(facecolor = '#31a354', edgecolor = 'black', linewidth = 0.5, label= 'TransExt')
-        ax.legend(handles = [No, cisint, transint, cisopp, transopp], bbox_to_anchor=(-10, 0.6), loc = 3, fontsize = 6, frameon = False, ncol = 5)
-             
+        intN = mpatches.Patch(facecolor = '#9ecae1', edgecolor = 'black', linewidth = 0.5, label= 'IntN')
+        intW = mpatches.Patch(facecolor = '#3182bd', edgecolor = 'black', linewidth = 0.5, label= 'IntW')
+        extN = mpatches.Patch(facecolor = '#a1d99b', edgecolor = 'black', linewidth = 0.5, label= 'ExtN')
+        extW = mpatches.Patch(facecolor = '#31a354', edgecolor = 'black', linewidth = 0.5, label= 'ExtW')
+        ax.legend(handles = [No, intN, intW, extN, extW], bbox_to_anchor=(-10, 0.6), loc = 3, fontsize = 6, frameon = False, ncol = 5)
+
 # adjust padding between subplots
 # pad controls the padding around the figure border
 # hpad and wpad control the padding between subplots
 plt.tight_layout(pad=0.2, w_pad=0, h_pad=0.5)
 
 fig.savefig('truc.pdf', bbox_inches = 'tight')
-
-#########
-
-
-# load dictionaries of host and nested genes 
-with open('HumanNestedGenes.json') as human_json_data:
-    Nested = json.load(human_json_data)
-
-# get GFF file
-GFF = 'Homo_sapiens.GRCh38.86.gff3'
-
-# get the coordinates of genes on each chromo
-# {chromo: {gene:[chromosome, start, end, sense]}}
-GeneChromoCoord = ChromoGenesCoord(GFF)
-# map each gene to its mRNA transcripts
-MapGeneTranscript = GeneToTranscripts(GFF)
-# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
-GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
-# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
-# Map Transcript names to gene names {transcript: gene}
-MapTranscriptGene = TranscriptToGene(GFF)
-# get the coordinates of all exons    
-ExonCoord = GeneExonCoord(GFF)
-ExonCoord = CleanGeneFeatureCoord(ExonCoord, MapTranscriptGene)
-# get the intron coordinates of all transcripts {transcript: [[intron_start, intron_end]]}
-IntronCoord = GeneIntronCoord(ExonCoord)
-IntronCoord = CleanGeneFeatureCoord(IntronCoord, MapTranscriptGene)
-# get the coordinates of each transcripts {transcript:[chromosome, start, end, sense]}
-TranscriptCoordinates = TranscriptsCoord(GFF)
-# map genes to their longest transcript {gene: longest_transcript}
-GeneLongestTranscript = LongestTranscript(TranscriptCoordinates, MapGeneTranscript)
-# match longest transcript of the nested genes to transcript of the host gene (longest transcript in priority)
-Matches = MatchHostTranscriptWithNestedTranscript(Nested, MapGeneTranscript, GeneLongestTranscript, TranscriptCoordinates, IntronCoord)
-# make a set of un-nested genes
-UnNestedGenes = MakeNonOverlappingGeneSet(Nested, GeneCoord)
-# list all host, nested transcript pairs [[host, nested]]
-HostNestedPairs = GetHostNestedPairs(Matches)
-
-
-# 1) plot the distribution of intron number per external gene 
-
-# make a set of external transcripts
-ExternalTS = list(Matches.keys())
-# make a set of external transcripts with intronless internal genes
-# make a set of external transcripts with intron-containing genes
-ExternalIntronless, ExternalWithIntron = set(), set()
-for i in range(len(HostNestedPairs)):
-    # get the external and internal transcripts of the pair
-    external, internal = HostNestedPairs[i][0], HostNestedPairs[i][1]
-    # check if internal transcript has intron
-    if internal in IntronCoord:
-        ExternalWithIntron.add(external)
-    else:
-        ExternalIntronless.add(external)
-
-# count the number of intron per external gene for all genes, external genes
-# with intronless internal genes and external genes with intron-containing internal genes
-TotalCount, IntronlessCount, WithIntronCount = [], [], []
-for gene in ExternalTS:
-    TotalCount.append(len(IntronCoord[gene]))
-for gene in ExternalIntronless:
-    IntronlessCount.append(len(IntronCoord[gene]))
-for gene in ExternalWithIntron:
-    WithIntronCount.append(len(IntronCoord[gene]))
-
-
-# 2) plot the distribution of intron position for gene-containing introns of external genes
-# 3) plot the distribution of intron position divided by the total number of introns
-
-IntronlessPos, WithIntronPos = [], []
-IntronlessPosNorm, WithIntronPosNorm = [], []
-
-for i in range(len(HostNestedPairs)):
-    # get the external and internal transcripts of the pair
-    external, internal = HostNestedPairs[i][0], HostNestedPairs[i][1]
-    assert TranscriptCoordinates[external][0] == TranscriptCoordinates[internal][0]
-    # find the position of the internal gene in its host gene
-    # get the start position of the internal gene
-    InternalStart = TranscriptCoordinates[internal][1]
-    # set boolean to check if intron is found
-    FoundIntron = False    
-    # check orientation of the external gene
-    assert TranscriptCoordinates[external][-1] in ['-', '+']    
-    if TranscriptCoordinates[external][-1] == '-':
-        # 1st intron of the list is last intron of transcript
-        # need to loop trhough introns in reversed order        
-        for j in range(len(IntronCoord[external]) -1, -1, -1):
-            # check if intron contains the internal gene
-            if InternalStart in range(IntronCoord[external][j][0], IntronCoord[external][j][1]):
-                # record position (1-based)
-                # check if internal gene is ontronless or not
-                if internal in IntronCoord:
-                    WithIntronPos.append(len(IntronCoord[external]) -j)
-                    WithIntronPosNorm.append((len(IntronCoord[external]) -j) / len(IntronCoord[external]))                    
-                else:
-                    IntronlessPos.append(len(IntronCoord[external]) -j)
-                    IntronlessPosNorm.append((len(IntronCoord[external]) -j) / len(IntronCoord[external]))
-            # update boolean
-            FoundIntron = True
-            # verify that internal gene is not in tron once intron has been found
-            if FoundIntron == False:
-                assert InternalStart not in range(IntronCoord[external][j][0], IntronCoord[external][j][1])
-    if TranscriptCoordinates[external][-1] == '+':
-        # loop over introns of the external gene    
-        for j in range(len(IntronCoord[external])):
-            # check if intron contains the internal gene
-            if InternalStart in range(IntronCoord[external][j][0], IntronCoord[external][j][1]):
-                # record position (1-based)
-                # check if internal gene is ontronless or not
-                if internal in IntronCoord:
-                    WithIntronPos.append(j+1)
-                    WithIntronPosNorm.append((j+1) / len(IntronCoord[external]))
-                else:
-                    IntronlessPos.append(j+1)
-                    IntronlessPosNorm.append((j+1) / len(IntronCoord[external]))                    
-                # update boolean
-                FoundIntron = True
-            # verify that internal gene is not in tron once intron has been found
-            if FoundIntron == False:
-                assert InternalStart not in range(IntronCoord[external][j][0], IntronCoord[external][j][1])
-  
-
-# sort lists
-WithIntronPosNorm = np.sort(WithIntronPosNorm)
-IntronlessPosNorm = np.sort(IntronlessPosNorm)
-# compute probabilities
-PWithPosNorm = np.array(range(len(WithIntronPosNorm))) / len(WithIntronPosNorm)
-PNonePosNorm = np.array(range(len(IntronlessPosNorm))) / len(IntronlessPosNorm) 
-
-# compare distributions of intron numbers, intron position
-PVals = []
-Data = [IntronlessCount, WithIntronCount, IntronlessPos, WithIntronPos, IntronlessPosNorm, WithIntronPosNorm]
-for i in range(0, len(Data), 2):
-    val, P =  stats.ks_2samp(Data[i], Data[i+1])
-    PVals.append(P)
-
-print(PVals)
-
-# get significance levels
-for i in range(len(PVals)):
-    if PVals[i] >= 0.05:
-        PVals[i] = ''
-    elif PVals[i] < 0.05 and PVals[i] >= 0.01:
-        PVals[i] = '$\it{P}$ < 0.05'
-    elif PVals[i] < 0.01 and PVals[i] >= 0.001:
-        PVals[i] = '$\it{P}$ < 0.01'
-    elif PVals[i] < 0.001:
-        PVals[i] = '$\it{P}$ < 0.001'
-
-print(PVals)  
-
-# create a function to format the subplots
-def CreateAx(Columns, Rows, Position, figure, Data, YLabel, XLabel, YRange, YMax, XRange, Colors, GraphType):
-    '''
-    (int, int, int, figure_object, list, list, list, list, list, str, str, int)
-    Take the number of a column, and rows in the figure object and the position of
-    the ax in figure, 2 lists of data, a list of bar positions, the list of tick
-    positions and their labels, a list of colors, a label for the Y axis,
-    a maximum value for the Y axis and return an ax instance in the figure
-    '''    
-    # create subplot in figure
-    # add a plot to figure (N row, N column, plot N)
-    ax = figure.add_subplot(Rows, Columns, Position)
-    # plot data    
-    if GraphType == 'histo':
-        ax.hist(Data[0], bins = np.arange(0, max([max(Data[0]), max(Data[1])]) + 1, 1), linewidth = 0.7, histtype='step', fill = True, facecolor = Colors[0], edgecolor = Colors[0], alpha = 0.5, stacked = False)    
-        ax.hist(Data[1], bins = np.arange(0, max([max(Data[0]), max(Data[1])]) + 1, 1), linewidth = 0.7, histtype='step', fill = True, facecolor = Colors[1], edgecolor = Colors[1], alpha = 0.5, stacked = False)
-    elif GraphType == 'cdf':
-        ax.step(Data[0], Data[1], linewidth = 1, linestyle = '-', color = Colors[0], alpha = 0.5)
-        ax.step(Data[2], Data[3], linewidth = 1, linestyle = '-', color = Colors[1], alpha = 0.5)
-        
-    # set font for all text in figure
-    FigFont = {'fontname':'Arial'}   
-    # write label axis
-    ax.set_ylabel(YLabel, color = 'black',  size = 6.5, ha = 'center', **FigFont)
-    ax.set_xlabel(XLabel, color = 'black',  size = 6.5, ha = 'center', **FigFont)
-    # set a limit to y axis
-    plt.ylim([0, YMax])
-    # add a range to the axis
-    plt.xticks(XRange, size = 6.5, color = 'black', ha = 'center', **FigFont)
-    plt.yticks(YRange, size = 6.5, color = 'black', ha = 'right', **FigFont)        
-    
-    # do not show lines around figure  
-    ax.spines["top"].set_visible(False)    
-    ax.spines["bottom"].set_visible(True)    
-    ax.spines["right"].set_visible(False)    
-    ax.spines["left"].set_visible(True)  
-    # edit tick paramters
-    plt.tick_params(axis='both', which='both', bottom='on', top='off', right = 'off',
-                    left = 'on', labelbottom='on', colors = 'black', labelsize = 7, direction = 'out')  
-    # add ticks on the x axis
-    #plt.xticks(TickPos, Ticklabel)    
-    # Set the tick labels font name
-    for label in ax.get_yticklabels():
-        label.set_fontname('Arial')   
-    # create a margin around the x axis
-    plt.margins(0.05)
-    return ax      
-
-# create figure
-fig = plt.figure(1, figsize = (2.5, 4.5))
-
-# plot distribution of intron number
-YMax1 = max(max([WithIntronCount.count(i) for i in WithIntronCount]),  max([IntronlessCount.count(i) for i in IntronlessCount])) + 10
-ax1 = CreateAx(1, 3, 1, fig, [WithIntronCount, IntronlessCount], 'N external genes', 'Number of introns', np.arange(0, YMax1 + 10, 10), YMax1, np.arange(0, 80, 10), ['orange', 'blue'], 'histo')
-# plot distribution of intron position
-YMax2 = max(max([WithIntronPos.count(i) for i in WithIntronPos]), max([IntronlessPos.count(i) for i in IntronlessPos])) + 10
-ax2 = CreateAx(1, 3, 2, fig, [WithIntronPos, IntronlessPos], 'N external genes', 'Intron position', np.arange(0, YMax2 + 20, 20), YMax2, np.arange(0, 60, 10), ['orange', 'blue'], 'histo')
-# plot distribution of intron position / intron number
-ax3 = CreateAx(1, 3, 3, fig, [WithIntronPosNorm, PWithPosNorm, IntronlessPosNorm, PNonePosNorm] , 'Probability', 'Intron position / intron number', np.arange(0, 1.1, 0.1), 1, np.arange(0, 1.1, 0.1), ['orange', 'blue'], 'cdf')
-
-# make sure subplots do not overlap
-plt.tight_layout()
-
-## annotate graphs with legends
-# create patches 
-Title = mpatches.Patch(facecolor = 'none', edgecolor = 'none', linewidth = 0, label= 'Internal genes:', alpha = 0)
-Yes = mpatches.Patch(facecolor = 'orange', edgecolor = 'none', linewidth = 0.5, label= 'with introns', alpha = 0.5)
-No = mpatches.Patch(facecolor = 'blue', edgecolor = 'black', linewidth = 0.5, label= 'intronless', alpha = 0.5)
-ax1.legend(handles = [Title, Yes, No], bbox_to_anchor=(0.2, 0.4), loc = 3, fontsize = 5, frameon = False, ncol = 1)
-ax2.legend(handles = [Title, Yes, No], bbox_to_anchor=(0.2, 0.4), loc = 3, fontsize = 5, frameon = False, ncol = 1)
-# create lines
-orange_line = mlines.Line2D([], [], color='orange', marker='', markersize=15, label='with introns', alpha = 0.5)
-blue_line = mlines.Line2D([], [], color='blue', marker='', markersize=15, label='intronless', alpha = 0.5)
-ax3.legend(handles = [Title, orange_line, blue_line], bbox_to_anchor=(0.05, 0.5), loc = 3, fontsize = 5, frameon = False, ncol = 1)
-
-# annotate graphs with p values
-for i in range(len(PVals)):
-    if PVals[i] != '':
-        if i == 0:
-            ax1.text(25, 20, PVals[i], color = 'black', size = 5, fontname = 'Arial')
-        elif i == 1:
-            ax2.text(15, 60, PVals[i], color = 'black', size = 5, fontname = 'Arial')
-        elif i == 2:
-            ax3.text(0.4, 0.3, PVals[i], color = 'black', size = 5, fontname = 'Arial')
 
