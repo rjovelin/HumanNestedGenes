c0f0b3db102e60effcbe161073c38afbaf44f205
Richard <richard.jovelin@oicr.on.ca>
2017-03-29 18:25:52 -0400

    added option to compute divergence with chimp or mouse orthologs

--- ComputeSeqDivergence.py
+++ ComputeSeqDivergence.py
@@ -8,36 +8,49 @@ Created on Tue Nov 29 23:30:44 2016
 
 # use this script to compute nucleotide divergence between human-chimp orthologs
 
-# usage python3 ComputeHumanChimpNuclDivergence.py
-
+# usage python3 ComputeSeqDivergence.py [options] 
+# -[mouse/chimp]: use orthologs in mouse or chimp
 
 import os
+import sys
 from HsaNestedGenes import *
 
+# get option from command
+Species = sys.argv[1]
+assert Species in ['mouse', 'chimp']
 
-# create a directory to 
-os.mkdir('HumanChimpCodeml')
+
+# assign source and destination folders
+if Species == 'chimp':
+    DestinationFolder = './HumanChimpCodeml/'
+    SourceFolder = './HumanChimpPairs/'    
+elif Species == 'mouse':
+    DestinationFolder = './HumanMouseCodeml/'
+    SourceFolder = './HumanMousePairs/'
+
+# create a destination folder to store the deivergence files
+os.mkdir(DestinationFolder)
 
 # convert alignment files to PAML format
 
 # get a list with the file names
-files = os.listdir('./HumanChimpPairs/')
+files = os.listdir(SourceFolder)
 for filename in files:
     # check that file is the t-coffee DNA alignment file
     if filename[-8:] == '_aln.tfa':
         # convert sequences to dictionnary
-        alignment = ConvertFasta('./HumanChimpPairs/' + filename)
+        alignment = ConvertFasta(SourceFolder + filename)
         # get the name of the file without '_aln.tfa'
         name = filename[:-8]
         # make a list with gene names as they appear in the file
         seq_names = []
-        infile = open('./HumanChimpPairs/' + filename)
+        infile = open(SourceFolder + filename)
         for line in infile:
             if line.startswith('>'):
                 seq_names.append(line.rstrip()[1:])
         infile.close()
         # open newfile for writing
-        newfile = open('HumanChimpCodeml/' + name + '.txt', 'w')
+        newfile = open(DestinationFolder + name + '.txt', 'w')
         # add the number of sequences and the length of the alignment in the first line
         newfile.write(str(len(alignment)) + ' ' + str(len(alignment[seq_names[0]])) + '\n')
         # add orthologous sequences in fasta format
@@ -50,7 +63,7 @@ print('done convertir alignments to PAML format')
 
 # generate codeml control files
 # get the list of alignment files
-files = os.listdir('./HumanChimpCodeml/')
+files = os.listdir(DestinationFolder)
 for filename in files:
     if filename[-4:] == '.txt' and 'ENS' in filename:
         # get the file name without extension including gene names
@@ -58,7 +71,7 @@ for filename in files:
         # open control file for reading
         ctlfile = open('codeml.ctl', 'r')
         # open outputfile file for writing
-        newfile = open('./HumanChimpCodeml/' + name + '_codeml.ctl', 'w')
+        newfile = open(DestinationFolder + name + '_codeml.ctl', 'w')
         # go through the control file, copy to new file with new input file and output file
         for line in ctlfile:
             if 'seqfile' not in line and 'outfile' not in line:
@@ -79,10 +92,10 @@ print('done generating codeml control files')
 
 # copy necessary files to directory with input and control files
 # copy files to working directory
-os.system('cp 2NG.dN 2NG.dS 2NG.t 4fold.nuc lnf HumanChimpTree.tre.txt rst rst1 rub ./HumanChimpCodeml/')
+os.system('cp 2NG.dN 2NG.dS 2NG.t 4fold.nuc lnf HumanChimpTree.tre.txt rst rst1 rub ' + DestinationFolder)
 
 # go to directory containing control files and input files
-os.chdir('./HumanChimpCodeml/')
+os.chdir(DestinationFolder)
 
 # run codeml
 # get the the list of control files
