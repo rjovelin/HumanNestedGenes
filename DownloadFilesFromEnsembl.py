2f1c48de0a268890fce5af6fbce768d7082a1d4f
Richard <richard.jovelin@oicr.on.ca>
2017-03-30 11:42:58 -0400

    added links to GFF and CDS files to download

--- DownloadFilesFromEnsembl.py
+++ DownloadFilesFromEnsembl.py
@@ -9,28 +9,30 @@ Created on Wed Mar 29 23:42:40 2017
 
 # use this script to download files from Ensembl
 
-
-# GFF files (for all species) and CDS files (for human, chimp and mouse) from ensembl
 # Note that web links were correct as of March 2017
-
-# baboon, gorilla, macaque, marmoset, orangoutan, bat, shrews, hedgehog, marsupial, platypus
 
 # import modules
 import os
 
-
 # download GFF files for all species of interest 
 # create a list of web links to GFF files
-GFFLinks = [
-'ftp://ftp.ensembl.org/pub/release-88/gff3/homo_sapiens/Homo_sapiens.GRCh38.88.gff3.gz',
+GFFLinks = ['ftp://ftp.ensembl.org/pub/release-88/gff3/homo_sapiens/Homo_sapiens.GRCh38.88.gff3.gz',
 'ftp://ftp.ensembl.org/pub/release-88/gff3/mus_musculus/Mus_musculus.GRCm38.88.gff3.gz',
 'ftp://ftp.ensembl.org/pub/release-88/gff3/felis_catus/Felis_catus.Felis_catus_6.2.88.gff3.gz',
 'ftp://ftp.ensembl.org/pub/release-88/gff3/pan_troglodytes/Pan_troglodytes.CHIMP2.1.4.88.gff3.gz',
 'ftp://ftp.ensembl.org/pub/release-88/gff3/bos_taurus/Bos_taurus.UMD3.1.88.gff3.gz',
-'ftp://ftp.ensembl.org/pub/release-88/gff3/canis_familiaris/Canis_familiaris.CanFam3.1.88.gff3.gz'
-
-
-]
+'ftp://ftp.ensembl.org/pub/release-88/gff3/canis_familiaris/Canis_familiaris.CanFam3.1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/gorilla_gorilla/Gorilla_gorilla.gorGor3.1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/erinaceus_europaeus/Erinaceus_europaeus.HEDGEHOG.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/equus_caballus/Equus_caballus.EquCab2.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/macaca_mulatta/Macaca_mulatta.Mmul_8.0.1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/callithrix_jacchus/Callithrix_jacchus.C_jacchus3.2.1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/monodelphis_domestica/Monodelphis_domestica.BROADO5.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/pongo_abelii/Pongo_abelii.PPYG2.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/ornithorhynchus_anatinus/Ornithorhynchus_anatinus.OANA5.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/choloepus_hoffmanni/Choloepus_hoffmanni.choHof1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/sorex_araneus/Sorex_araneus.COMMON_SHREW1.88.gff3.gz',
+'ftp://ftp.ensembl.org/pub/release-88/gff3/dasypus_novemcinctus/Dasypus_novemcinctus.Dasnov3.0.88.gff3.gz']
 
 # loop of urls and download the corresponding file
 for link in GFFLinks:
