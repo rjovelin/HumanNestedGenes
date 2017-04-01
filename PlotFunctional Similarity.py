b37b0bd6dd4716b5a8ad513bf96c30ae46f3de69
Richard <richard.jovelin@oicr.on.ca>
2017-03-31 22:12:18 -0400

    initial commit

---
+++ PlotFunctional Similarity.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 21:24:45 2017
+
+@author: Richard
+"""
+
+# use this script to plot differences in functional similarities between overlapping genes
+
+
+# import modules
+# use Agg backend on server without X server
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib import rc
+rc('mathtext', default='regular')
+import json
+import random
+import copy
+import sys
+import os
+import math
+import numpy as np
+from scipy import stats
+from HsaNestedGenes import *
+
+
+
+# load the overlapping gene dictionaries
+
+
+# make a set of non-overlapping genes
+
+
+
+
+
