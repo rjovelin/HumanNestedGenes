6fd1e610ba4d6d380fd087d5c669d219d26ae2b2
Richard <richard.jovelin@oicr.on.ca>
2017-04-02 17:53:08 -0400

    compute functional similarity between members of the same gene pair

--- PlotFunctional Similarity.py
+++ PlotFunctional Similarity.py
@@ -62,9 +62,34 @@ GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
 # Order genes along chromo {chromo: [gene1, gene2, gene3...]} 
 OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
 
-
 # make pairs of overlapping genes
 OverlappingPairs = []
+for i in range(len(Overlap)):
+    pairs = GetHostNestedPairs(Overlap[i])
+    OverlappingPairs.append(pairs)
+
+# make a set of non-overlapping genes
+NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlap[0], GeneCoord)
+
+# map gene names with gene ID {gene ID: name}
+Names = MapNametoID(GFF)
+# parse file with GO annotations 
+GOAnnotations = ParseGOFile('goa_human.gaf')
+# map gene IDs to GO annotations            
+GeneOntology = MapEnsemblGenesToGOTerms(GOAnnotations, Names)
+
+
+# create a list of lists with functional similarities between genes of the same pair
+FunctionalSimilarity = []
+for i in range(len(OverlappingPairs)):
+    # compute jaccard similarity index between each pair
+    GOOverlap = []
+    for pair in OverlappingPairs[i]:
+        JI = JaccardIndex(pair[0], pair[1])
+        GOOverlap.append(JI)
+    FunctionalSimilarity.append(GOOverlap)
+        
+        
 
 
 
@@ -74,21 +99,25 @@ OverlappingPairs = []
 
 
 
-for i in range(len(AllOverlap)):
-    pairs = GetHostNestedPairs(AllOverlap[i])
-    AllPairs.append(pairs)
-# make pairs of overlapping genes
-HumanPairs = AllPairs[:2]
-SisterPairs = AllPairs[2:4]
-OutGroupPairs = AllPairs[4:]
 
-# make list with sets of non-overlapping genes
-NonOverlappingSets = []
-for i in range(3):
-    j = i * 2
-    # make a set of non-overlapping gene
-    nonoverlap = MakeNonOverlappingGeneSet(AllOverlap[j], AllCoordinates[i])
-    NonOverlappingSets.append(nonoverlap)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######################################
+
+
+
+
 
 # make sets of host and nested nested genes
 NestedSets = []
@@ -102,60 +131,9 @@ for i in range(0, len(AllOverlap), 2):
     overlap = MakeFullPartialOverlapGeneSet(AllOverlap[i])
     OverlapSets.append(overlap)
 
-# get 1:1 orthologs between human and sister-species
-# get 1:1 orthologs between human, sister-species and outgroup {human:[sistersp,outgroup]}
-if SisterSp == 'chimp':
-    OrthoPairs = MatchOrthologPairs('HumanChimpOrthologs.txt')
-    OrthoTrios = MatchOrthologTrios('HumanChimpGorillaOrthologs.txt')
-elif SisterSp == 'mouse':
-    OrthoPairs = MatchOrthologPairs('HumanMouseOrthologs.txt')
-    OrthoTrios = MatchOrthologTrios('HumanMouseDogOrthologs.txt')
-
-# reverse dict with human and sister-species orthologs
-SisterOrthos = {}
-for gene in OrthoPairs:
-    SisterOrthos[OrthoPairs[gene]] = gene
-# make a dict of ortho trios with sister-species genes as key
-SisterOrthoTrios = {}
-for gene in OrthoTrios:
-    sistergene, outgroupgene = OrthoTrios[gene][0], OrthoTrios[gene][1]
-    SisterOrthoTrios[sistergene] = [gene, outgroupgene]
 
 
-# infer young and old nesting events 
-HumanOld, HumanYoung = InferYoungOldNestingEvents(OrthoPairs, OrthoTrios, SisterPairs[1], OutGroupPairs[1], HumanPairs[1])
-SisterSpOld, SisterSpYoung = InferYoungOldNestingEvents(SisterOrthos, SisterOrthoTrios, HumanPairs[1], OutGroupPairs[1], SisterPairs[1])
 
-# do some QC   
-a = [set([OrthoPairs[pair[0]], OrthoPairs[pair[1]]]) for pair in HumanOld]
-b = [set(pair) for pair in SisterSpOld]
-assert len(a) == len(b)
-for i in a:
-    assert i in b    
-
-# get expression profiles of human and sister-species
-if SisterSp == 'chimp':
-    HumanExpression = ParsePrimateExpressionData('NormalizedRPKM_ConstitutiveExons_Primate1to1Orthologues.txt', 'human')
-    SisterSpExpression = ParsePrimateExpressionData('NormalizedRPKM_ConstitutiveExons_Primate1to1Orthologues.txt', 'chimp')
-elif SisterSp == 'mouse':
-    HumanExpression = ParseExpressionFile('GTEX_Median_Normalized_FPKM.txt')
-    SisterSpExpression = ParseExpressionFile('Mouse_Median_Normalized_FPKM.txt')
-    # match expression profiles between mouse and human 
-    HumanExpression = MatchHumanToMouseExpressionProfiles(HumanExpression)
-
-# remove genes without expression
-HumanExpression = RemoveGenesLackingExpression(HumanExpression)
-SisterSpExpression = RemoveGenesLackingExpression(SisterSpExpression)
-# get relative expression
-HumanExpression = TransformRelativeExpression(HumanExpression)
-SisterSpExpression = TransformRelativeExpression(SisterSpExpression)
-
-# compute expression specificity
-HumanSpecificity = ExpressionSpecificity(HumanExpression)
-SisterSpSpecificity = ExpressionSpecificity(SisterSpExpression)
-
-HumanInferredPairs = [HumanOld, HumanYoung]
-SisterSpInferredPairs = [SisterSpOld, SisterSpYoung]
 
 
 
