469e416c9ba33bf9ad38bc7e7136bde1d98a1573
Richard <richard.jovelin@oicr.on.ca>
2017-04-03 18:23:55 -0400

    added option to use only specific GO ids

--- PlotFunctionalSimilarity.py
+++ PlotFunctionalSimilarity.py
@@ -7,6 +7,8 @@ Created on Fri Mar 31 21:24:45 2017
 
 # use this script to plot differences in functional similarities between overlapping genes
 
+# usage PlotFunctionalSimilarity
+#- [all/molfunc/celcomp/biolproc]: use all GO terns, molecular function, cellular compartments or biological processes
 
 # import modules
 # use Agg backend on server without X server
@@ -28,10 +30,11 @@ from HsaNestedGenes import *
 
 
 
-# load the overlapping gene dictionaries
+#get option from command
+GOClass = sys.argv[1]
+assert GOClass in ['all', 'molfunc', 'celcomp', 'biolproc']
 
 
-# make a set of non-overlapping genes
 
 # load dictionaries of overlapping genes
 JsonFiles = ['HumanOverlappingGenes.json', 'HumanNestedGenes.json',
@@ -73,11 +76,23 @@ NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlap[0], GeneCoord)
 
 # map gene names with gene ID {gene ID: name}
 Names = MapNametoID(GFF)
+
 # parse file with GO annotations 
 GOAnnotations = ParseGOFile('goa_human.gaf')
 # map gene IDs to GO annotations            
 GeneOntology = MapEnsemblGenesToGOTerms(GOAnnotations, Names)
 
+# check option to see GO ids are filtered or not
+if GOClass == 'molfunc':
+    GOSubSet = FilterGOTerms('GOMolecularFunction.txt')
+elif GOClass == 'celcomp':
+    GOSubSet = FilterGOTerms('GOCellularCompartments.txt')
+elif GOClass == 'biolproc':
+    GOSubSet = FilterGOTerms('GOBiologicalProcesses.txt')
+# keep only terms for specific GO class
+if GOClass != 'all':
+    for gene in GeneOntology:
+         GeneOntology[gene] = set(filter(lambda x: x in GOSubSet, GeneOntology[gene]))
 
 # create a list of lists with functional similarities between genes of the same pair
 FunctionalSimilarity = []
@@ -85,32 +100,35 @@ for i in range(len(OverlappingPairs)):
     # compute jaccard similarity index between each pair
     GOOverlap = []
     for pair in OverlappingPairs[i]:
-        JI = JaccardIndex(GeneOntology[pair[0]], GeneOntology[pair[1]])
-        GOOverlap.append(JI)
+        if pair[0] in GeneOntology and pair[1] in GeneOntology:
+            JI = JaccardIndex(GeneOntology[pair[0]], GeneOntology[pair[1]])
+            GOOverlap.append(JI)
     FunctionalSimilarity.append(GOOverlap)
-        
-        
+ 
 # generate random pairs of non-overlapping genes as a control
-# convert set of non-overlapping genes to list
+# make a list of non-overlapping genes convert set of non-overlapping genes to list
 NonOverlappingGenes = list(NonOverlappingGenes)
 ControlPairs = []
 # draw 10000 random pairs
-for i in range(10000):
+replicates = 10000
+while replicates != 0:
     # pick 2 random genes
     j = random.randint(0, len(NonOverlappingGenes) -1)
     k = random.randint(0, len(NonOverlappingGenes) -1)
     gene1, gene2 = NonOverlappingGenes[j], NonOverlappingGenes[k]
-    JI = JaccardIndex(GeneOntology[gene1], GeneOntology[gene2])
-    ControlPairs.append(JI)
-
+    if gene1 in GeneOntology and gene2 in GeneOntology:
+        JI = JaccardIndex(GeneOntology[gene1], GeneOntology[gene2])
+        ControlPairs.append(JI)
+        replicates -= 1
+  
 # insert pairs of JI in list
 FunctionalSimilarity.insert(0, ControlPairs)
 
-for i in range(len(FunctionalSimilarity)):
-    print(np.mean(FunctionalSimilarity[i]))
+DataSets = ['Control', 'Overlapping', 'Nested', 'PiggyBack', 'Convergent', 'Divergent']
 
-
-
+for i in range(1, len(FunctionalSimilarity)):
+    P = PermutationResampling(FunctionalSimilarity[0], FunctionalSimilarity[i], 1000, statistic = np.mean)
+    print(DataSets[0], DataSets[i], len(FunctionalSimilarity[0]), len(FunctionalSimilarity[i]), np.mean(FunctionalSimilarity[0]), np.mean(FunctionalSimilarity[i]), P)
 
 
 
