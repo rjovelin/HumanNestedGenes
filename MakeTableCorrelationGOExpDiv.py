61efc2cc692c889c499bfae49e57801830613293
Richard <richard.jovelin@oicr.on.ca>
2017-04-24 17:47:15 -0400

    write correlations to file

--- MakeTableCorrelationGOExpDiv.py
+++ MakeTableCorrelationGOExpDiv.py
@@ -5,11 +5,9 @@ Created on Tue Apr  4 14:51:38 2017
 @author: RJovelin
 """
 
-# use this script to plot the correlation between expression divergence between gene pairs and functional similarity
+# use this script to make a table with correlation between expression divergence and functional similarity among gene pairs
 
-# usage python3 PlotCorrelationGOExpDiv.py [options]
-# #- [all/molfunc/celcomp/biolproc]: use all GO terms, molecular function, cellular compartments or biological processes
-
+# usage python3 MakeTableCorrelationGOExpDiv.py 
 
 # import modules
 # use Agg backend on server without X server
@@ -30,17 +28,12 @@ from scipy import stats
 from HsaNestedGenes import *
 
 
-#get option from command
-GOClass = sys.argv[1]
-assert GOClass in ['all', 'molfunc', 'celcomp', 'biolproc']
-
 # load dictionaries of overlapping genes
 JsonFiles = ['HumanOverlappingGenes.json', 'HumanNestedGenes.json',
              'HumanPiggyBackGenes.json', 'HumanConvergentGenes.json',
              'HumanDivergentGenes.json']
 # get GFF file
 GFF = 'Homo_sapiens.GRCh38.88.gff3'
-
 # make a list of dictionaries
 Overlap = []
 # loop over files
@@ -60,15 +53,12 @@ MapGeneTranscript = GeneToTranscripts(GFF)
 GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
 # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
 GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
-# Order genes along chromo {chromo: [gene1, gene2, gene3...]} 
-OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
 
 # make pairs of overlapping genes
 OverlappingPairs = []
 for i in range(len(Overlap)):
     pairs = GetHostNestedPairs(Overlap[i])
     OverlappingPairs.append(pairs)
-
 # make a set of non-overlapping genes
 NonOverlappingGenes = MakeNonOverlappingGeneSet(Overlap[0], GeneCoord)
 
@@ -78,22 +68,7 @@ Names = MapNametoID(GFF)
 GOAnnotations = ParseGOFile('goa_human.gaf')
 # map gene IDs to GO annotations            
 GeneOntology = MapEnsemblGenesToGOTerms(GOAnnotations, Names)
-# check option to see GO ids are filtered or not
-if GOClass == 'molfunc':
-    GOSubSet = FilterGOTerms('GOMolecularFunction.txt')
-elif GOClass == 'celcomp':
-    GOSubSet = FilterGOTerms('GOCellularCompartments.txt')
-elif GOClass == 'biolproc':
-    GOSubSet = FilterGOTerms('GOBiologicalProcesses.txt')
-# keep only terms for specific GO class
-if GOClass != 'all':
-    for gene in GeneOntology:
-         GeneOntology[gene] = set(filter(lambda x: x in GOSubSet, GeneOntology[gene]))
-# remove genes if gene has no GO term
-to_remove = [gene for gene in GeneOntology if GeneOntology[gene] == 0]
-if len(to_remove) != 0:
-    for gene in to_remove:
-        del GeneOntology[gene]
+a = len(GeneOntology)
 
 # get the expression profile of each gene
 Expression = ParseExpressionFile('GTEX_Median_Normalized_FPKM.txt')
@@ -102,118 +77,146 @@ Expression = RemoveGenesLackingExpression(Expression)
 # get relative expression
 Expression = TransformRelativeExpression(Expression)
 
-
-# make a list of non-overlapping genes convert set of non-overlapping genes to list
-NonOverlappingGenes = list(NonOverlappingGenes)
-# delete genes that do not have any GO or that are not expressed
-to_remove = [gene for gene in NonOverlappingGenes if gene not in GeneOntology or gene not in Expression]
-for gene in to_remove:
-    NonOverlappingGenes.remove(gene)
-print(len(NonOverlappingGenes))
-
-# generate random pairs of non-overlapping genes and compute JI and expression divergence
-# stote JI and expdiv in parrallel lists [[JI], [exodiv]]
-BaseLine = [[], []]
-# draw 10000 random pairs
-replicates = 10000
-while replicates != 0:
-    # pick 2 random genes
-    j = random.randint(0, len(NonOverlappingGenes) -1)
-    k = random.randint(0, len(NonOverlappingGenes) -1)
-    gene1, gene2 = NonOverlappingGenes[j], NonOverlappingGenes[k]
-    # compute functional similarity index
-    JI = JaccardIndex(GeneOntology[gene1], GeneOntology[gene2])
-    # compute expression divergence        
-    expdiv = EuclidianDistance(Expression[gene1], Expression[gene2])
-    BaseLine[0].append(JI)
-    BaseLine[1].append(expdiv)    
-    replicates -= 1
-     
-# remove pairs if genes do not have GO terms
-for i in range(len(OverlappingPairs)):
-    to_remove = [pair for pair in OverlappingPairs[i] if pair[0] not in GeneOntology or pair[1] not in GeneOntology]
-    for pair in to_remove:
-        OverlappingPairs[i].remove(pair)
-# filter gene pairs lacking expression
-for i in range(len(OverlappingPairs)):
-    OverlappingPairs[i] = FilterGenePairsWithoutExpression(OverlappingPairs[i], Expression)
-
-# create parallel lists of functional similarity and expression divergence for each type of overalapping gene
-FunctionalSimilarity, ExpressionDivergence = [], []
-
-# include only the 4 overlapping types
-for i in range(1, len(OverlappingPairs)):
-    # compute jaccard similarity index  and expression divergence between each pair    
-    GOOverlap, ExpDiv = [], []
-    for pair in OverlappingPairs[i]:
-        JI = JaccardIndex(GeneOntology[pair[0]], GeneOntology[pair[1]])
-        expdiv = EuclidianDistance(Expression[pair[0]], Expression[pair[1]])
-        GOOverlap.append(JI)
-        ExpDiv.append(expdiv)
-    FunctionalSimilarity.append(GOOverlap)
-    ExpressionDivergence.append(ExpDiv)
+# make a dict of correlation coefficients for each GO class {all: [baseline, nsted, pbk, conv, div], ...'biolproc':[baseline, nsted, pbk, conv, div]}
+Correlations = {}
+for GOClass in ['all', 'molfunc', 'celcomp', 'biolproc']:
+    # make a set of GO ontology terms
+    if GOClass == 'molfunc':
+        GOSubSet = FilterGOTerms('GOMolecularFunction.txt')
+    elif GOClass == 'celcomp':
+        GOSubSet = FilterGOTerms('GOCellularCompartments.txt')
+    elif GOClass == 'biolproc':
+        GOSubSet = FilterGOTerms('GOBiologicalProcesses.txt')
+    # copy GeneOntology dict
+    GeneGOTerms = copy.deepcopy(GeneOntology)
+    assert GeneGOTerms == GeneOntology
+    assert a == len(GeneGOTerms)    
+    # keep only terms for specific GO class
+    if GOClass != 'all':
+        for gene in GeneGOTerms:
+            GeneGOTerms[gene] = set(filter(lambda x: x in GOSubSet, GeneGOTerms[gene]))
+    # remove genes if gene has no GO term
+    to_remove = [gene for gene in GeneGOTerms if len(GeneGOTerms[gene]) == 0]
+    if len(to_remove) != 0:
+        for gene in to_remove:
+            del GeneGOTerms[gene]
+    # make a list of non-overlapping genes that have GO and that are expressed            
+    NonOverlapping = [gene for gene in NonOverlappingGenes if gene in GeneGOTerms and gene in Expression]
+        
+    # generate random pairs of non-overlapping genes and compute JI and expression divergence
+    # store JI and expdiv in parrallel lists [[JI], [exodiv]]
+    BaseLine = [[], []]
+    # draw 10000 random pairs
+    replicates = 10000
+    while replicates != 0:
+        # pick 2 random genes
+        j = random.randint(0, len(NonOverlapping) -1)
+        k = random.randint(0, len(NonOverlapping) -1)
+        gene1, gene2 = NonOverlapping[j], NonOverlapping[k]
+        # compute functional similarity index
+        JI = JaccardIndex(GeneOntology[gene1], GeneOntology[gene2])
+        # compute expression divergence        
+        expdiv = EuclidianDistance(Expression[gene1], Expression[gene2])
+        BaseLine[0].append(JI)
+        BaseLine[1].append(expdiv)    
+        replicates -= 1
     
-# do some QC
-for i in range(len(FunctionalSimilarity)):
-    assert len(FunctionalSimilarity[i]) == len(ExpressionDivergence[i])
+    # initialize dict with correlation between expression divergence and functional similarity for the random genes to list
+    a, b = stats.spearmanr(BaseLine[0], BaseLine[1])
+    Correlations[GOClass] = [[a, b]]      
+    
+    # include only the 4 overlapping types    
+    for i in range(1, len(OverlappingPairs)):
+        # make a list of list of overlapping pairs that have GO terms and that are expressed
+        OvlPairs = [pair for pair in OverlappingPairs[i] if (pair[0] in GeneOntology and pair[1] in GeneOntology) and (pair[0] in Expression and pair[1] in Expression)]
+        # compute jaccard similarity index  and expression divergence between each pair    
+        GOOverlap, ExpDiv = [], []
+        for pair in OvlPairs:
+            JI = JaccardIndex(GeneGOTerms[pair[0]], GeneGOTerms[pair[1]])
+            expdiv = EuclidianDistance(Expression[pair[0]], Expression[pair[1]])
+            GOOverlap.append(JI)
+            ExpDiv.append(expdiv)
+        # compute correlation between functional similarity and expression divergence
+        a, b = stats.spearmanr(GOOverlap, ExpDiv)
+        Correlations[GOClass].append([a, b])        
+        
+# open file to store the results of the correlation in table
+newfile = open('CorrelationsGOExpDiv.txt', 'w')
+
+# create table
+newfile.write('Table 1. Correlation between expression divergence and Gene Ontology similarity\n')
+newfile.write('\t'.join(['GO Class', 'Random', 'Nested', 'PiggyBack', 'Convergent', 'DivergentGenes']) + '\n')
+
+GOTypes = ['all', 'molfunc', 'celcomp', 'biolproc']
+GONames = ['All', 'Molecular Function', 'Cellular Compartments', 'Biological Processes']
+for i in range(len(GOTypes)):
+    line = [GONames[i]]
+    for correl in Correlation[GOTypes[i]]:
+        line.append(str(correl[0]) + ' (' + str(correl[1]) + ')')
+    line = '\t'.join(line)
+    newfile.write(line + '\n')
+# close file after writing
+newfile.close()
 
 
-# create a function to format the subplots
-def CreateAx(Columns, Rows, Position, figure, Data, Colors, Title):
-    '''
-    (int, int, int, figure_object, list, list, list, list, list, str, str, int)
-    Take the number of a column, and rows in the figure object and the position of
-    the ax in figure, 2 lists of data, a list of bar positions, the list of tick
-    positions and their labels, a list of colors, a label for the Y axis,
-    a maximum value for the Y axis and return an ax instance in the figure
-    '''    
-    # create subplot in figure
-    # add a plot to figure (N row, N column, plot N)
-    ax = figure.add_subplot(Rows, Columns, Position)
-    # plot data
-    ax.scatter(Data[0], Data[1], marker = 'o', edgecolor = Colors, facecolor = Colors, lw = 0.5, s = 4, alpha = 0.5)
-    # set font for all text in figure
-    FigFont = {'fontname':'Arial'}   
-    # write axis label
-    ax.set_ylabel('Expression divergence', color = 'black',  size = 7, ha = 'center', **FigFont)
-    ax.set_xlabel('Functional similarity', color = 'black',  size = 7, ha = 'center', **FigFont)
-#    # add ticks and labels
-#    ax.set_xticklabels([0.15, 0.45, 0.75, 1.05, 1.35, 1.65, 1.95, 2.25], GeneCats, size = 7, color = 'black', ha = 'center', **FigFont)
-#    # add a range to the axis
-#    plt.xticks(XRange, size = 6.5, color = 'black', ha = 'center', **FigFont)
-#    plt.yticks(YRange, size = 6.5, color = 'black', ha = 'right', **FigFont)       
 
-    # add a range for the Y and X axes
-#    plt.ylim([0, 1.5])
-#    plt.xlim([0, 500000])
-    # add title
-    ax.set_title(Title, size = 7, ha = 'center', **FigFont)    
-    # do not show lines around figure  
-    ax.spines["top"].set_visible(False)    
-    ax.spines["bottom"].set_visible(True)    
-    ax.spines["right"].set_visible(False)
-    ax.spines["left"].set_visible(True)  
-    # edit tick parameters    
-    plt.tick_params(axis='both', which='both', bottom='on', top='off',
-                    right = 'off', left = 'on', labelbottom='on',
-                    colors = 'black', labelsize = 7, direction = 'out')  
-    # Set the tick labels font name
-    for label in ax.get_yticklabels():
-        label.set_fontname('Arial')   
-    return ax
-
-# create figure
-fig = plt.figure(1, figsize = (5, 4))
-
-ax1 = CreateAx(1, 5, 1, fig, BaseLine, 'black', 'random')
-ax2 = CreateAx(1, 5, 2, fig, [FunctionalSimilarity[0], ExpressionDivergence[0]], 'black', 'nested')
-ax3 = CreateAx(1, 5, 3, fig, [FunctionalSimilarity[1], ExpressionDivergence[1]], 'black', 'piggyback')
-ax4 = CreateAx(1, 5, 4, fig, [FunctionalSimilarity[2], ExpressionDivergence[2]], 'black', 'convergent')
-ax5 = CreateAx(1, 5, 5, fig, [FunctionalSimilarity[3], ExpressionDivergence[3]], 'black', 'divergent')
-
-# make sure subplots do not overlap
-plt.tight_layout()
-
-# save figure
-fig.savefig('truc.pdf', bbox_inches = 'tight')
-
+#
+## create a function to format the subplots
+#def CreateAx(Columns, Rows, Position, figure, Data, Colors, Title):
+#    '''
+#    (int, int, int, figure_object, list, list, list, list, list, str, str, int)
+#    Take the number of a column, and rows in the figure object and the position of
+#    the ax in figure, 2 lists of data, a list of bar positions, the list of tick
+#    positions and their labels, a list of colors, a label for the Y axis,
+#    a maximum value for the Y axis and return an ax instance in the figure
+#    '''    
+#    # create subplot in figure
+#    # add a plot to figure (N row, N column, plot N)
+#    ax = figure.add_subplot(Rows, Columns, Position)
+#    # plot data
+#    ax.scatter(Data[0], Data[1], marker = 'o', edgecolor = Colors, facecolor = Colors, lw = 0.5, s = 4, alpha = 0.5)
+#    # set font for all text in figure
+#    FigFont = {'fontname':'Arial'}   
+#    # write axis label
+#    ax.set_ylabel('Expression divergence', color = 'black',  size = 7, ha = 'center', **FigFont)
+#    ax.set_xlabel('Functional similarity', color = 'black',  size = 7, ha = 'center', **FigFont)
+##    # add ticks and labels
+##    ax.set_xticklabels([0.15, 0.45, 0.75, 1.05, 1.35, 1.65, 1.95, 2.25], GeneCats, size = 7, color = 'black', ha = 'center', **FigFont)
+##    # add a range to the axis
+##    plt.xticks(XRange, size = 6.5, color = 'black', ha = 'center', **FigFont)
+##    plt.yticks(YRange, size = 6.5, color = 'black', ha = 'right', **FigFont)       
+#
+#    # add a range for the Y and X axes
+##    plt.ylim([0, 1.5])
+##    plt.xlim([0, 500000])
+#    # add title
+#    ax.set_title(Title, size = 7, ha = 'center', **FigFont)    
+#    # do not show lines around figure  
+#    ax.spines["top"].set_visible(False)    
+#    ax.spines["bottom"].set_visible(True)    
+#    ax.spines["right"].set_visible(False)
+#    ax.spines["left"].set_visible(True)  
+#    # edit tick parameters    
+#    plt.tick_params(axis='both', which='both', bottom='on', top='off',
+#                    right = 'off', left = 'on', labelbottom='on',
+#                    colors = 'black', labelsize = 7, direction = 'out')  
+#    # Set the tick labels font name
+#    for label in ax.get_yticklabels():
+#        label.set_fontname('Arial')   
+#    return ax
+#
+## create figure
+#fig = plt.figure(1, figsize = (5, 4))
+#
+#ax1 = CreateAx(1, 1, 1, fig, BaseLine, 'black', 'random')
+#ax2 = CreateAx(1, 5, 2, fig, [FunctionalSimilarity[0], ExpressionDivergence[0]], 'black', 'nested')
+#ax3 = CreateAx(1, 5, 3, fig, [FunctionalSimilarity[1], ExpressionDivergence[1]], 'black', 'piggyback')
+#ax4 = CreateAx(1, 5, 4, fig, [FunctionalSimilarity[2], ExpressionDivergence[2]], 'black', 'convergent')
+#ax5 = CreateAx(1, 5, 5, fig, [FunctionalSimilarity[3], ExpressionDivergence[3]], 'black', 'divergent')
+#
+## make sure subplots do not overlap
+#plt.tight_layout()
+#
+## save figure
+#fig.savefig('truc.pdf', bbox_inches = 'tight')
+#
