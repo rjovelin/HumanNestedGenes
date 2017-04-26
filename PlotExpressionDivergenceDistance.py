a3fb4e7400ed125dad260ea6b2ca90087b9aec12
Richard <richard.jovelin@oicr.on.ca>
2017-04-26 13:12:15 -0400

    edited GFF file version

--- PlotExpressionDivergenceDistance.py
+++ PlotExpressionDivergenceDistance.py
@@ -51,7 +51,7 @@ for i in range(len(Overlap)):
 
 
 # get GFF file
-GFF = 'Homo_sapiens.GRCh38.86.gff3'
+GFF = 'Homo_sapiens.GRCh38.88.gff3'
 # get the coordinates of genes on each chromo
 # {chromo: {gene:[chromosome, start, end, sense]}}
 GeneChromoCoord = ChromoGenesCoord(GFF)
