c34902c975877a2e8940d04aad35215ef9108205
Richard <richard.jovelin@oicr.on.ca>
2017-04-27 12:42:51 -0400

    edited x label

--- PlotIntronComparison.py
+++ PlotIntronComparison.py
@@ -180,7 +180,7 @@ fig = plt.figure(1, figsize = (4.5, 2))
 # plot data for intron numner
 ax1 = CreateAx(3, 1, 1, fig, NumMeans, NumSEM, [0, 0.2, 0.4], [0.1, 0.3, 0.5], ['Ext', 'Int', 'Not'], ['grey','black','white'], 'Introns / gene', 20)
 ax2 = CreateAx(3, 1, 2, fig, LengthMeans, LengthSEM, [0, 0.2, 0.4], [0.1, 0.3, 0.5], ['Ext', 'Int', 'Not'], ['grey','black','white'], 'Intron length (Kbp)', 20)
-ax3 = CreateAx(3, 1, 3, fig, HostIntronMeans, HostIntronSEM, [0, 0.2], [0.1, 0.3], ['With', 'None'], ['grey','black'], 'Intron length (Kbp)', 70)
+ax3 = CreateAx(3, 1, 3, fig, HostIntronMeans, HostIntronSEM, [0, 0.2], [0.1, 0.3], ['With\nInt', 'Without\nInt'], ['grey','black'], 'Intron length (Kbp)', 70)
 
 # make lists with bracket and star positions
 XPosNum = [[0.1, 0.28, 13, 0.2, 13.7], [0.1, 0.5, 14, 0.3, 15.2], [0.32, 0.5, 13, 0.4, 13.7]]
