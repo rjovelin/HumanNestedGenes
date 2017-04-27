9f4159d59140cdbb108420d2cf35b9dafa89b8ee
Richard <richard.jovelin@oicr.on.ca>
2017-04-27 13:44:18 -0400

    updated GFF version

--- PlotGeneLengthDiff.py
+++ PlotGeneLengthDiff.py
@@ -43,7 +43,7 @@ for i in range(len(JsonFiles)):
     Overlap.append(overlapping)
 
 # get GFF file
-GFF = 'Homo_sapiens.GRCh38.86.gff3'
+GFF = 'Homo_sapiens.GRCh38.88.gff3'
 # get the coordinates of genes on each chromo
 # {chromo: {gene:[chromosome, start, end, sense]}}
 GeneChromoCoord = ChromoGenesCoord(GFF)
