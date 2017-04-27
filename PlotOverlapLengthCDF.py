d86a2996735f70d1bdd552d7dab74b955b8b7484
Richard <richard.jovelin@oicr.on.ca>
2017-04-27 18:22:54 -0400

    edited figure size and subplot location

--- PlotOverlapLengthCDF.py
+++ PlotOverlapLengthCDF.py
@@ -56,233 +56,62 @@ MapGeneTranscript = GeneToTranscripts(GFF)
 GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
 # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
 GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
+# Order genes along chromo {chromo: [gene1, gene2, gene3...]} 
+OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
+# make a set of non-overlapping genes
+NonOverlapping = MakeNonOverlappingGeneSet(Overlap[0], GeneCoord)
 
-# create lists of gene pairs
-OverlappingPairs = GetHostNestedPairs(OverlappingGenes)
-NestedPairs = GetHostNestedPairs(NestedGenes)
-PiggybackPairs = GetHostNestedPairs(Piggyback)
-ConvergentPairs = GetHostNestedPairs(Convergent)
-DivergentPairs = GetHostNestedPairs(Divergent)
+# make pairs of overlapping genes
+OverlappingPairs = []
+for i in range(len(Overlap)):
+    pairs = GetHostNestedPairs(Overlap[i])
+    OverlappingPairs.append(pairs)
 
-# create a list of lists of gene pairs
-AllPairs = [OverlappingPairs, NestedPairs, PiggybackPairs, ConvergentPairs, DivergentPairs]
+
+
+# 1) plot the distribution of overlap length between overlapping gene pairs
+
 # create a parallel list of overlap length
-AllLength = []
-# loop over lists of gene pairs for each overlapping group
-for i in range(len(AllPairs)):
+LengthData = []
+# loop over lists of gene pairs for the 4 overlapping groups of interest
+for i in range(1, len(OverlappingPairs)):
     # initialize empty list
     Length = []
-    for pair in AllPairs[i]:
-        assert GeneCoord[pair[0]][0] == GeneCoord[pair[1]][0]
+    for pair in OverlappingPairs[i]:
+        assert GeneCoord[pair[0]][0] == GeneCoord[pair[1]][0], 'chromosome should be the same'
         # get the coordinates of the gene pairs
         coord1 = set(range(GeneCoord[pair[0]][1], GeneCoord[pair[0]][2]))
         coord2 = set(range(GeneCoord[pair[1]][1], GeneCoord[pair[1]][2]))
         L = len(coord1.intersection(coord2))
         Length.append(L)
     # store lists of overlap length
-    AllLength.append(Length)
-# get the list of overlap length for each group
-OverlapLength, NestedLength = AllLength[0], AllLength[1]
-PiggybackLength, ConvergentLength, DivergentLength = AllLength[2], AllLength[3], AllLength[4]
+    LengthData.append(Length)
 
 # convert bp to Kbp
 ToKb = lambda x: x / 1000
-OverlapLength = list(map(ToKb, OverlapLength))
-NestedLength = list(map(ToKb, NestedLength))
-PiggybackLength = list(map(ToKb, PiggybackLength))
-ConvergentLength = list(map(ToKb, ConvergentLength))
-DivergentLength = list(map(ToKb, DivergentLength))
+for i in range(len(LengthData)):
+    LengthData[i] = list(map(ToKb, LengthData[i]))
 
-def CombineHighValues(L, cutoff):
-    '''
-    (list, int) -> list
-    Take a list of overlap length and return a modified list with values higher
-    than cutoff equal to cutoff
-    '''
-    for i in range(len(L)):
-        if L[i] >= cutoff:
-            L[i] = cutoff
-    return L
-
-OverlapLength = CombineHighValues(OverlapLength, 200)
-NestedLength = CombineHighValues(NestedLength, 200)
-PiggybackLength = CombineHighValues(PiggybackLength, 200)
-ConvergentLength = CombineHighValues(ConvergentLength, 200)
-DivergentLength = CombineHighValues(DivergentLength, 200)
-
-# check that lists are different
-assert OverlapLength != NestedLength != PiggybackLength != ConvergentLength != DivergentLength
-
+# bin values > 200 kb into the same bin
+for i in range(len(LengthData)):
+    LengthData[i] = CombineHighValues(LengthData[i], 200)
+    
 # sort lists
-OverlapLength = np.sort(OverlapLength)
-NestedLength = np.sort(NestedLength)
-PiggybackLength = np.sort(PiggybackLength)
-ConvergentLength = np.sort(ConvergentLength)
-DivergentLength = np.sort(DivergentLength)
-
+for i in range(len(LengthData)):
+    LengthData[i] = np.sort(LengthData[i])
 # compute probabilities
-POverlap = np.array(range(len(OverlapLength))) / len(OverlapLength)
-PNested = np.array(range(len(NestedLength))) / len(NestedLength) 
-PPiggy = np.array(range(len(PiggybackLength))) / len(PiggybackLength)
-PConvergent = np.array(range(len(ConvergentLength))) / len(ConvergentLength)
-PDivergent = np.array(range(len(DivergentLength))) / len(DivergentLength)
+ProbaLength = []
+for i in range(len(LengthData)):
+    ProbaLength.append(np.array(range(len(LengthData[i]))) / len(LengthData[i]))
 
-# create figure
-fig = plt.figure(1, figsize = (3, 2))
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)  
-
-# make a list of colors
-Colors = ['#d7191c', '#fdae61', '#abd9e9', '#2c7bb6']
-# make a list of line styles
-LineStyle = ['-', '--', ':', '-.']
-
-# plot nested length
-graph1 = ax.step(NestedLength, PNested, linewidth = 1.2, linestyle = '-', color = 'black')
-# plot pibbyback length
-graph2 = ax.step(PiggybackLength, PPiggy, linewidth = 1.2, linestyle = '--', color = 'black')
-# plot convergent length
-graph3 = ax.step(ConvergentLength, PConvergent, linewidth = 1.2, linestyle = '-', color = 'grey')
-# plot divergent length
-graph4 = ax.step(DivergentLength, PDivergent, linewidth = 1.2, linestyle = '--', color = 'grey')
-
-# add label for the Y axis
-ax.set_ylabel('Probability', size = 8, ha = 'center', fontname = 'Arial')
-# set x axis label
-ax.set_xlabel('Overlap length (Kb)', size = 8, ha = 'center', fontname = 'Arial')
-# set x axis ticks
-plt.xticks([0, 50, 100, 150, 200], ['0', '50', '100', '150', r'$\geq 200$'])
-
-
-# do not show lines around figure, keep bottow line  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(True)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(True)      
-
-ax.tick_params(
-    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'on',          
-    labelbottom='on', # labels along the bottom edge are off 
-    colors = 'black',
-    labelsize = 8,
-    direction = 'out') # ticks are outside the frame when bottom = 'on
-
-for label in ax.get_yticklabels():
-    label.set_fontname('Arial')
-
-# add lines
-lns = graph1+graph2+graph3+graph4
-# get labels
-labs = ['Nested', 'Piggyback', 'Convergent', 'Divergent']
-# plot legend
-ax.legend(lns, labs, loc=4, fontsize = 8, frameon = False)
-
-
-# compare distributions based using Kolmogorov-Smirnov statistic on 2 samples.
-
-# make a list of overlapping length
-Data = [NestedLength, PiggybackLength, ConvergentLength, DivergentLength]
-
-for i in range(0, len(Data) -1):
-    for j in range(i+1, len(Data)):
-        val, P =  stats.ks_2samp(Data[i], Data[j])
-        print(labs[i], labs[j], val, P)
+# compute P values among groups
+for i in range(0, len(LengthData) -1):
+    for j in range(i+1, len(LengthData)):
+        val, P =  stats.ks_2samp(LengthData[i], LengthData[j])
+        print(i, j, val, P)
         
-fig.savefig('OverlapLengthCDF.pdf', bbox_inches = 'tight')
-fig.savefig('OverlapLengthCDF.eps', bbox_inches = 'tight')
 
-
-
-
-##############################
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 21 17:59:04 2016
-
-@author: RJovelin
-"""
-
-# use this script to plot the CDF of the ration of the length of the shorter gene 
-# over the length of the longer gene for overlapping genes and non-overlapping gene neighbors
-
-# usage python3 PlotLengthRatio.py
-
-# import modules
-# use Agg backend on server without X server
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from matplotlib import rc
-rc('mathtext', default='regular')
-import json
-import random
-import copy
-import sys
-import os
-import math
-import numpy as np
-from scipy import stats
-from HsaNestedGenes import *
-
-
-# load dictionary of overlapping gene pairs
-json_data = open('HumanOverlappingGenes.json')
-OverlappingGenes = json.load(json_data)
-json_data.close()
-
-# load dictionary of nested gene pairs
-json_data = open('HumanNestedGenes.json')
-NestedGenes = json.load(json_data)
-json_data.close()
-
-# load dictionary of pibbyback gene pairs
-json_data = open('HumanPiggyBackGenes.json')
-Piggyback = json.load(json_data)
-json_data.close()
-
-# load dictionary of convergent gene pairs
-json_data = open('HumanConvergentGenes.json')
-Convergent = json.load(json_data)
-json_data.close()
-
-# load dictionary of divergent gene pairs
-json_data = open('HumanDivergentGenes.json')
-Divergent = json.load(json_data)
-json_data.close()
-
-
-# get GFF file
-GFF = 'Homo_sapiens.GRCh38.86.gff3'
-# get the coordinates of genes on each chromo
-# {chromo: {gene:[chromosome, start, end, sense]}}
-GeneChromoCoord = ChromoGenesCoord(GFF)
-# map each gene to its mRNA transcripts
-MapGeneTranscript = GeneToTranscripts(GFF)
-# remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
-GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
-# get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
-# Order genes along chromo {chromo: [gene1, gene2, gene3...]} 
-OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
-
-# make a set of non-overlapping genes
-NonOverlapping = MakeNonOverlappingGeneSet(OverlappingGenes, GeneCoord)
-
-# create lists of gene pairs
-OverlappingPairs = GetHostNestedPairs(OverlappingGenes)
-NestedPairs = GetHostNestedPairs(NestedGenes)
-PiggybackPairs = GetHostNestedPairs(Piggyback)
-ConvergentPairs = GetHostNestedPairs(Convergent)
-DivergentPairs = GetHostNestedPairs(Divergent)
+# 2) plot the CDF of the ratio of the length of the shorter gene over the length of the longer gene for overlapping genes and non-overlapping gene neighbors
 
 # create pairs of non-overlapping gene neighbors
 NeighborsSame, NeighborsOpposite = [], []
@@ -305,7 +134,7 @@ for chromo in OrderedGenes:
 # create pairs of nested genes with same and opposite orientation
 NestedSame, NestedOpposite = [], []
 # loop over nested pairs
-for pair in NestedPairs:
+for pair in OverlappingPairs[1]:
     if GeneCoord[pair[0]][-1] != GeneCoord[pair[1]][-1]:
         # opposite direction
         NestedOpposite.append(pair)
@@ -313,11 +142,8 @@ for pair in NestedPairs:
         # same direction
         NestedSame.append(pair)
 
-# create a list of lists of gene pairs
-#AllPairs = [NestedPairs, PiggybackPairs, ConvergentPairs, DivergentPairs]
-
 # create list of gene pairs
-AllPairs = [NestedSame, NestedOpposite, PiggybackPairs, ConvergentPairs, DivergentPairs, NeighborsSame, NeighborsOpposite]
+AllPairs = [NestedSame, NestedOpposite, OverlappingPairs[2], OverlappingPairs[3], OverlappingPairs[4], NeighborsSame, NeighborsOpposite]
 # create a parallel list of length ratio
 Ratios = []
 # Compute ratios of length (in %) of shorter gene over length of longer genes
@@ -344,11 +170,15 @@ for i in range(len(Ratios)):
     P = np.array(range(len(Ratios[i]))) / len(Ratios[i])
     Proba.append(P)
 
-# create figure
-fig = plt.figure(1, figsize = (6, 2))
+# make lists of data and probabilitties for same-strand and opposite strand overlapping genes
+SameStrdData = [Ratios[0], Ratios[2], Ratios[5]]
+SameStrdProba = [Proba[0], Proba[2], Proba[5]]
+OppositeStrdData = [Ratios[1], Ratios[3], Ratios[4], Ratios[6]]
+OppositeStrdProba = [Proba[1], Proba[3], Proba[4], Proba[6]]
+
 
 # create a function to format the subplots
-def CreateAx(Columns, Rows, Position, figure, Data, Proba, LineStyle, Labels, Title):
+def CreateAx(Columns, Rows, Position, figure, Data, Proba, Labels, YLabel, XLabel, TickPos, TickLabel, Colors, AddTitle = False, Title = ''):
     '''
     (int, int, int, figure_object, list, list, list, list, list, str, str, int)
     Take the number of a column, and rows in the figure object and the position of
@@ -361,63 +191,42 @@ def CreateAx(Columns, Rows, Position, figure, Data, Proba, LineStyle, Labels, Ti
     ax = figure.add_subplot(Rows, Columns, Position)
     # plot data
     for i in range(len(Data)):
-        if i == len(Data) -1:
-            graph = ax.step(Data[i], Proba[i], linewidth = 1.2, linestyle = '-', color = 'grey')
-        else:
-            graph = ax.step(Data[i], Proba[i], linewidth = 1.2, linestyle = LineStyle[i], color = 'black')
+        graph = ax.step(Data[i], Proba[i], linewidth = 1.2, linestyle = '-', color = Colors[i])
         if i == 0:
             lns = graph
         else:
             lns += graph
-    
     # set title
     FigFont = {'fontname':'Arial'}   
-    plt.title(Title, size = 8, color = 'black', ha = 'center', **FigFont )     
-    # add label for the Y axis
-    ax.set_ylabel('Probability', size = 8, ha = 'center', **FigFont)
-    # set x axis label
-    ax.set_xlabel('Length ratio (%)', size = 8, ha = 'center', **FigFont)
-    # set x axis ticks
-    plt.xticks([i for i in range(0, 110, 10)], [str(i) for i in range(0, 110, 10)])
+    if AddTitle == True:
+        plt.title(Title, size = 8, color = 'black', ha = 'center', **FigFont )     
     
+    # add label for the Y axis
+    ax.set_ylabel(YLabel, size = 8, ha = 'center', **FigFont)
+    # set x axis label
+    ax.set_xlabel(XLabel, size = 8, ha = 'center', **FigFont)
+    # set x axis ticks
+    plt.xticks(TickPos, TickLabel)
     # do not show lines around figure, keep bottow line  
     ax.spines["top"].set_visible(False)    
     ax.spines["bottom"].set_visible(True)    
     ax.spines["right"].set_visible(False)    
     ax.spines["left"].set_visible(True)      
-
-    ax.tick_params(
-        axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
-        which='both',      # both major and minor ticks are affected
-        bottom='on',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        right = 'off',
-        left = 'on',          
-        labelbottom='on', # labels along the bottom edge are off 
-        colors = 'black',
-        labelsize = 8,
-        direction = 'out') # ticks are outside the frame when bottom = 'on
-    
+    # edit tick parameters
+    ax.tick_params(axis='both', which='both', bottom='on', top='off', right = 'off',
+        left = 'on', labelbottom='on', colors = 'black', labelsize = 8, direction = 'out')
     for label in ax.get_yticklabels():
         label.set_fontname('Arial')
-
     # get labels
     labs = Labels
     # plot legend
     ax.legend(lns, labs, loc=4, fontsize = 8, frameon = False)
-    
-#    # add x axis ticks
-#    plt.xticks([i for i in range(0, 120, 20)], [str(i) for i in range(0, 120, 20)])
-#    # add y axis ticks
-#    plt.yticks([i for i in range(0, 120, 20)], [str(i) for i in range(0, 120, 20)])
- 
     return ax     
 
-# make lists of data and probabilitties for same-strand and opposite strand overlapping genes
-SameStrdData = [Ratios[0], Ratios[2], Ratios[5]]
-SameStrdProba = [Proba[0], Proba[2], Proba[5]]
-OppositeStrdData = [Ratios[1], Ratios[3], Ratios[4], Ratios[6]]
-OppositeStrdProba = [Proba[1], Proba[3], Proba[4], Proba[6]]
+
+
+# create figure
+fig = plt.figure(1, figsize = (5, 4))
 
 # compare distributions based using Kolmogorov-Smirnov statistic on 2 samples.
 Label1 = ['Nested', 'Piggyback', 'Non-overlapping']
@@ -444,11 +253,11 @@ for i in range(0, len(OppositeStrdData) -1):
     elif P < 0.001:
         Label2[i] = Label2[i] + ' ***'
 
-# create a list of line styles
-LineStyle = ['-', '--', ':', '-.']
 # create subplots    
-ax1 = CreateAx(2, 1, 1, fig, SameStrdData, SameStrdProba, LineStyle, Label1, 'Same strand')
-ax2 = CreateAx(2, 1, 2, fig, OppositeStrdData, OppositeStrdProba, LineStyle, Label2, 'Opposite strand')
+ax1 = CreateAx(1, 2, 1, fig, LengthData, ProbaLength, ['Nested', 'Piggyback', 'Convergent', 'Divergent'], 'Probability', 'Overlap length (Kb)', [0, 50, 100, 150, 200], ['0', '50', '100', '150', r'$\geq 200$'], ['#f03b20', '#43a2ca', '#fee391', '#74c476'])
+ax2 = CreateAx(2, 2, 2, fig, SameStrdData, SameStrdProba, Label1, 'Probability', 'Length ratio (%)', [i for i in range(0, 110, 10)], [str(i) for i in range(0, 110, 10)], ['#f03b20', '#43a2ca', 'lightgrey'], True, 'Same strand')
+ax3 = CreateAx(2, 2, 4, fig, OppositeStrdData, OppositeStrdProba, Label2, 'Probability', 'Length ratio (%)', [i for i in range(0, 110, 10)], [str(i) for i in range(0, 110, 10)], ['#f03b20', '#fee391', '#74c476', 'lightgrey'], True, 'Opposite strand')
+
 
 # add subplot labels
 ax1.text(-10, 1.1, 'A', horizontalalignment='center', verticalalignment='center',
@@ -456,5 +265,4 @@ ax1.text(-10, 1.1, 'A', horizontalalignment='center', verticalalignment='center'
 ax1.text(110, 1.1, 'B', horizontalalignment='center', verticalalignment='center',
          color = 'black', fontname = 'Arial', size = 9)
     
-fig.savefig('LengthRatioCDF.pdf', bbox_inches = 'tight')
-fig.savefig('LengthRatioCDF.eps', bbox_inches = 'tight')
+fig.savefig('truc.pdf', bbox_inches = 'tight')
