7051637b12720c8e32f8f901a35a22bb264e2f06
Richard <richard.jovelin@oicr.on.ca>
2017-05-03 12:25:16 -0400

    updated GFF file version

--- PlotOverlappingGeneDensity.py
+++ PlotOverlappingGeneDensity.py
@@ -47,7 +47,7 @@ Overlapping = json.load(json_data)
 json_data.close()
 
 # get GFF file
-GFF = 'Homo_sapiens.GRCh38.86.gff3'
+GFF = 'Homo_sapiens.GRCh38.88.gff3'
 # get the coordinates of genes on each chromo
 # {chromo: {gene:[chromosome, start, end, sense]}}
 GeneChromoCoord = ChromoGenesCoord(GFF)
