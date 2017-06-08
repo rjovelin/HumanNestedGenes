dba32e58778b98625a3c122d8f0fd3c619230d9f
Richard <richard.jovelin@oicr.on.ca>
2017-06-08 11:35:33 -0400

    edited figure size

--- DvptPlotOverlapCons.py
+++ DvptPlotOverlapCons.py
@@ -462,9 +462,8 @@ def CreateAx(NColumns, NRows, Grid1, Grid2, RowPos,ColPos, figure, gs, Data, Gra
       
     return ax
 
-
 # create figure
-figure = plt.figure(1, figsize = (8, 5))
+figure = plt.figure(1, figsize = (6.8, 3.8))
 # set up grid
 gs = gridspec.GridSpec(2, 3, width_ratios=[3, 1]) 
 # plot data
