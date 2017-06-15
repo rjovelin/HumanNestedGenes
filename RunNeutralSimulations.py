fbd7d42c69305289beb460d1143700ca6567de07
Richard <richard.jovelin@oicr.on.ca>
2017-06-15 18:48:19 -0400

    increase walltime

--- RunNeutralSimulations.py
+++ RunNeutralSimulations.py
@@ -18,7 +18,7 @@ newfile.write('\n')
 newfile.write('python3 SimulationsNeutralOverlap.py')
 newfile.close()            
 
-MyCommand = 'qsub -l walltime=10:00:00,nodes=1:ppn=12,mem=46g ' + outputfile
+MyCommand = 'qsub -l walltime=20:00:00,nodes=1:ppn=12,mem=46g ' + outputfile
 os.system(MyCommand)
 print(MyCommand)
         
