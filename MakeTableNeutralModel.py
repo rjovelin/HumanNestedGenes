1379c84e573442a7159dad419328aafe9b532599
Richard <richard.jovelin@oicr.on.ca>
2017-06-15 19:26:42 -0400

    compare observed counts to neutral expectations

--- MakeTableNeutralModel.py
+++ MakeTableNeutralModel.py
@@ -27,12 +27,17 @@ from scipy import stats
 from HsaNestedGenes import *
 
 
+# load dictionary of counts of overlapping and non-overlapping genes from simulations
+infile = open('SimulationsOverlap.json')  
+Simulations = json.load(infile)
+infile.close()
+
 # make a parallel list of Species names
 Species = ['Human', 'Chimp', 'Gorilla', 'Orangutan',
            'Macaque', 'Marmoset', 'Hedgehog', 'Shrew',
            'Cat', 'Dog', 'Mouse', 'Cow', 'Horse', 'Sloth',
            'Armadillo', 'Opossum', 'Platypus']
-
+# make a list a GFF files
 GFF = ['Homo_sapiens.GRCh38.88.gff3', 'Pan_troglodytes.CHIMP2.1.4.88.gff3',
        'Gorilla_gorilla.gorGor3.1.88.gff3', 'Pongo_abelii.PPYG2.88.gff3',
        'Macaca_mulatta.Mmul_8.0.1.88.gff3', 'Callithrix_jacchus.C_jacchus3.2.1.88.gff3',
@@ -43,18 +48,8 @@ GFF = ['Homo_sapiens.GRCh38.88.gff3', 'Pan_troglodytes.CHIMP2.1.4.88.gff3',
        'Dasypus_novemcinctus.Dasnov3.0.88.gff3', 'Monodelphis_domestica.BROADO5.88.gff3',
        'Ornithorhynchus_anatinus.OANA5.88.gff3']
 
-
 # make a list with gene coordinates in each species {gene:[chromosome, start, end, sense]}
 SpGeneCoordinates = []
-# make a list with gene corodinates on each chromo in each species {chromo: {gene:[chromosome, start, end, sense]}}
-SpChromoGeneCoord = []
-# make a list with ordered genes in each chromo
-SpOrderedGenes = []
-# make a list with chromosome length for each species
-SpChromoLength = []
-
-
-# populate lists
 for i in range(len(GFF)):
     # get the coordinates of genes on each chromo {chromo: {gene:[chromosome, start, end, sense]}}
     GeneChromoCoord = ChromoGenesCoord(GFF[i])
@@ -62,17 +57,9 @@ for i in range(len(GFF)):
     MapGeneTranscript = GeneToTranscripts(GFF[i])
     # remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
     GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
-    SpChromoGeneCoord.append(GeneChromoCoord)    
     # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
     GeneCoord = FromChromoCoordToGeneCoord(GeneChromoCoord)
     SpGeneCoordinates.append(GeneCoord)    
-    # odrder genes along chromo {chromo: [gene1, gene2, gene3...]} 
-    OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
-    SpOrderedGenes.append(OrderedGenes)
-    # get chromosome length
-    ChromoL = GetChromosomeLength(GFF[0])
-    SpChromoLength.append(ChromoL)
-    
     
 # count the number of observed overlapping and non-overlapping genes in each species {sp: [overlap, non-overlap]}
 ObservedOverlaGenes = {}
@@ -87,37 +74,26 @@ for i in range(len(Species)):
     # count the number of non-overlapping genes, store in list
     ObservedOverlaGenes[Species[i]].append(len(MakeNonOverlappingGeneSet(overlapping, SpGeneCoordinates[i])))    
 
-for i in range(len(Species)):
-    print(Species[i], ObservedOverlaGenes[Species[i]])
+# make a dict with mean and standard error from simulations
+Mean = {}, SEM = {}
+for species in Simulations:
+    NO = [Simulations[species][0] for species in Simulations]
+    NN = [Simulations[species][1] for species in Simulations]
+    Mean[species] = [np.mean(NO), np.mean(NN)]    
+    SEM[species] = [np.std(NO) / math.sqrt(len(NO)), np.std(NN) / math.sqrt(len(NN))]     
+        
+# compute P values of differences between observations and expectations
+PVals = {}
+for species in ObservedOverlaGenes:
+    P = stats.fisher_exact([ObservedOverlaGenes[species], Mean[species]])[1]     
+    PVals[species] = P
+    
+for species in ObservedOverlaGenes:
+    print(species, ObservedOverlaGenes[species][0], ObservedOverlaGenes[species][1], Mean[species][0], Mean[species][1], PVals[species])
     
     
     
-for i in range(3):
-    # get the coordinates of human genes on each chromo
-    # {chromo: {gene:[chromosome, start, end, sense]}}
-    GeneChromoCoord = ChromoGenesCoord(GFF[0])
-    # get the length of each chromosome
-    ChromoLength = GetChromosomeLength(GFF[0])
-    # map each gene to its mRNA transcripts
-    MapGeneTranscript = GeneToTranscripts(GFF[0])
-    # remove genes that do not have a mRNA transcripts (may have abberant transcripts, NMD processed transcripts, etc)
-    GeneChromoCoord = FilterOutGenesWithoutValidTranscript(GeneChromoCoord, MapGeneTranscript)
-    # order genes along chromosomes    
-    OrderedGenes = OrderGenesAlongChromo(GeneChromoCoord)
-    # randomize gene positions along each chromo
-    RandomCoord = RandomizeGenePosition(GeneChromoCoord, ChromoLength)
-    # get the coordinates of each gene {gene:[chromosome, start, end, sense]}
-    RandomGeneCoord = FromChromoCoordToGeneCoord(RandomCoord)    
-    # order randomized genes on each chromo
-    OrderedRandom = OrderGenesAlongChromo(RandomCoord)
-    # find overlapping genes from randomized genes
-    OverlapRandom = FindOverlappingGenePairs(RandomCoord, OrderedRandom)
-    # make a set of overlapping genes
-    ovlp = MakeFullPartialOverlapGeneSet(OverlapRandom)
-    # make a set of non-overlapping gene
-    nonovlp = MakeNonOverlappingGeneSet(ovlp, RandomGeneCoord)    
-    print(i, len(ovlp), len(nonovlp))
-
-  
-
-
+    
+    
+    
+    
