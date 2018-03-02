38d7f7658bf285956e35650508e9c63ab9e64ad2
rjovelin <richard.jovelin@utoronto.ca>
2018-03-01 22:18:12 -0500

    pass # simulation runs and walltime as arguments

--- RunNeutralSimulations.py
+++ RunNeutralSimulations.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 16 12:33:35 2018
-
 @author: RJovelin
 """
 
 import os
+import sys
+import getopt
 
 # use this script to generate shell scripts to run neutral simulations and launch jobs
+
+
+def Usage():
+    print("""
+    usage: RunNeutralSimulations.py [-h|--Help|-w|--Walltime|-i|--Iterations]
+    -h, --Help: help
+    -w, --Walltime: job run time in hours (96)
+    -i, --Iterations: number of simulation runs
+    """)
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hw:i:', ['Help', 'Walltime=', 'Iterations='])
+except getopt.GetoptError:
+    Usage()
+    sys.exit(2)
+else:
+    for opt, val in opts:
+        if opt in ('-h', '--Help'):
+            Usage()
+            sys.exit(2)
+        elif opt in ('-w', '--Walltime'):
+            Walltime = val
+        elif opt in ('-i', '--Iterations'):
+            Iterations = int(val)
 
 # make a list of species
 Species = ['Human', 'Chimp', 'Gorilla', 'Orangutan', 'Macaque', 'Marmoset',
@@ -15,8 +40,6 @@ Species = ['Human', 'Chimp', 'Gorilla', 'Orangutan', 'Macaque', 'Marmoset',
            'Armadillo', 'Opossum', 'Platypus']
 # make a list of models to use
 Models = ['randomization', 'extension']
-# set number of simulations
-Iterations = 500
 
 # write shell script to launch the job
 # loop over models
@@ -30,7 +53,6 @@ for model in Models:
         newfile.write('\n')
         newfile.write('python3 NeutralSimulations.py ' + model + ' ' + species + ' ' + str(Iterations))
         newfile.close()            
-        MyCommand = 'qsub -l walltime=72:00:00,nodes=1:ppn=12,mem=46g ' + outputfile
+        MyCommand = 'qsub -l walltime={0}:00:00,nodes=1:ppn=12,mem=46g '.format(Walltime) + outputfile
         os.system(MyCommand)
         print(MyCommand)
-        
